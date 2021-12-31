2a83694add7d27fb65477b2888a335856f1b1dae
Ivan Gao <ivan.gao@cnosdb.com>
2021-12-31 17:17:58 +0800

    chore(*): Increase the packaging process by docker.
    This commit can use docker to build a meta node and data node images

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-if [ "${1:0:1}" = '-' ]; then
-    set -- cnosdb "$@"
+if [ "$1" = 'cnosdb' ]; then
+	/init-cnosdb.sh "${@}"
 fi
 
-if [ "$1" = 'cnosdb' ]; then
-	/init-cnosdb.sh "${@:2}"
+if [ "$1" = 'cnosdb-meta' ]; then
+	/init-cnosdb.sh "${@}"
+fi
+
+if [ "$1" = 'cnosdb-data' ]; then
+	/init-cnosdb.sh "${@}"
 fi
 
 exec "$@"
