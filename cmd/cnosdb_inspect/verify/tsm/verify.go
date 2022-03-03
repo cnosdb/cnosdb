69efccc9323721613d788b53c7085a83dac8af66
Victor <1511224829@qq.com>
2022-03-03 19:45:27 +0800

    feat(cmd): update feature verify

--- cmd/cnosdb_inspect/verify/tsm/verify.go
+++ cmd/cnosdb_inspect/verify/tsm/verify.go
@@ -1,9 +1,12 @@
 package verify
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
 	"io"
 	"os"
+	"os/user"
+	"path"
 )
 
 type Options struct {
@@ -23,7 +26,26 @@ func NewOptions() *Options {
 var opt = NewOptions()
 
 func GetCommand() *cobra.Command {
-	return nil
+	c := &cobra.Command{
+		Use:   "verify",
+		Short: "Verifies the integrity of TSM files.",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := verify(cmd, args); err != nil {
+				fmt.Println(err)
+			}
+		},
+	}
+	var defaultDir string
+	u, err := user.Current()
+	if err == nil {
+		defaultDir = path.Join(u.HomeDir, ".cnosdb")
+	} else if os.Getenv("HOME") != "" {
+		defaultDir = path.Join(os.Getenv("HOME"), ".cnosdb")
+	} else {
+		defaultDir = ""
+	}
+	c.PersistentFlags().StringVar(&opt.path, "dir", defaultDir, "The path to the storage root directory.")
+	return c
 }
 
 func verify(cmd *cobra.Command, args []string) error {
