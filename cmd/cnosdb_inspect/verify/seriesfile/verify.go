6af9051dbccd09398094318d6fd3ff09fcb6fc99
Victor <1511224829@qq.com>
2022-03-04 19:07:43 +0800

    feat(cmd): update feature verify-seriesfile

--- cmd/cnosdb_inspect/verify/seriesfile/verify.go
+++ cmd/cnosdb_inspect/verify/seriesfile/verify.go
@@ -1,10 +1,28 @@
 package seriesfile
 
+import (
+	"go.uber.org/zap"
+)
+
+// Verify contains configuration for running verification of series files.
 type Verify struct {
+	Concurrent int
+	Logger     *zap.Logger
+
+	done chan struct{}
 }
 
-func NewVerify() *Verify {
-	return &Verify{}
+func NewVerify(concurrent int) *Verify {
+	return &Verify{
+		Concurrent: concurrent,
+		Logger:     zap.NewNop(),
+	}
+}
+
+// verifyResult contains the result of a Verify___ call
+type verifyResult struct {
+	valid bool
+	err   error
 }
 
 func (v Verify) VerifySeriesFile(filePath string) (valid bool, err error) {
