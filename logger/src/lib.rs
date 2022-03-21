34d6dd3e7b14b90f7005cde32b0ec88c406df8cd
vrg000 <97928905+vrg000@users.noreply.github.com>
2022-03-21 10:12:45 +0800

    add logger crate

---
+++ logger/src/lib.rs
@@ -0,0 +1,20 @@
+pub use log::{info, debug, warn, trace, error, LevelFilter};
+use simplelog::{Config, SharedLogger, TerminalMode,
+                ColorChoice, WriteLogger, CombinedLogger, TermLogger};
+use std::fs::File;
+
+pub fn init(level: LevelFilter, file_path: &str) {
+    let mut logger = Vec::<Box<dyn SharedLogger>>::new();
+    let config = Config::default();
+
+    logger.push(TermLogger::new(
+        level, config.clone(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto));
+
+    // file
+    logger.push(WriteLogger::new(
+        level, config.clone(),
+        File::create(file_path).unwrap()));
+    CombinedLogger::init(logger).unwrap();
+}
