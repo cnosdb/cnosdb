d6de8e5b2254e8a1bafbdb4d7de154345f74deed
Ivan Gao <92921813+IvanGao01@users.noreply.github.com>
2022-03-23 17:24:01 +0800

    ci: Update build for docker

--- cmd/cnosdb-meta/options/options.go
+++ cmd/cnosdb-meta/options/options.go
@@ -14,8 +14,8 @@ var Env = options{}
 // GetConfigPath returns the config path from the options.
 // It will return a path by searching in this order:
 //   1. The CLI option in ConfigPath
-//   2. The environment variable CNOSDB_CONFIG_PATH
-//   3. The first cnosdb.conf file on the path:
+//   2. The environment variable CNOSDB_META_CONFIG_PATH
+//   3. The first cnosdb-meta.conf file on the path:
 //        - ~/.cnosdb
 //        - /etc/cnosdb
 func (opt options) GetConfigPath() string {
@@ -24,13 +24,13 @@ func (opt options) GetConfigPath() string {
 			return ""
 		}
 		return opt.ConfigFile
-	} else if envVar := os.Getenv("CNOSDB_CONFIG_PATH"); envVar != "" {
+	} else if envVar := os.Getenv("CNOSDB_META_CONFIG_PATH"); envVar != "" {
 		return envVar
 	}
 
 	for _, path := range []string{
-		os.ExpandEnv("${HOME}/.cnosdb/cnosdb.conf"),
-		"/etc/cnosdb/cnosdb.conf",
+		os.ExpandEnv("${HOME}/.cnosdb/cnosdb-meta.conf"),
+		"/etc/cnosdb/cnosdb-meta.conf",
 	} {
 		if _, err := os.Stat(path); err == nil {
 			return path
