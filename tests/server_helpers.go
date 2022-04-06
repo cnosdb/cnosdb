48d9bc4566117ae4806a8d2aa7ff536e519e4fb2
gilbert-liang <gilbert.liang@cnosdb.com>
2022-04-06 17:11:15 +0800

    add struct config && server

--- tests/server_helpers.go
+++ tests/server_helpers.go
@@ -2,10 +2,16 @@
 package tests
 
 import (
-	"github.com/cnosdb/cnosdb/meta"
-	"github.com/cnosdb/cnosdb/vend/db/models"
+	"github.com/cnosdb/cnosdb/vend/common/pkg/toml"
 	"io"
 	"net/url"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/cnosdb/cnosdb/meta"
+	"github.com/cnosdb/cnosdb/server"
+	"github.com/cnosdb/cnosdb/vend/db/models"
 )
 
 var verboseServerLogs bool
@@ -34,4 +40,40 @@ type Server interface {
 	Write(db, rp, body string, params url.Values) (results string, err error)
 	MustWrite(db, rp, body string, params url.Values) string
 	WritePoints(database, retentionPolicy string, consistencyLevel models.ConsistencyLevel, user meta.User, points []models.Point) error
+}
+
+func NewServer(c *Config) {
+
+}
+
+type Config struct {
+	rootPath string
+	*server.Config
+}
+
+func NewConfig() *Config {
+	root, err := os.MkdirTemp("", "tests-cnosdb-")
+	if err != nil {
+		panic(err)
+	}
+
+	c := &Config{rootPath: root, Config: server.NewConfig()}
+	c.BindAddress = "127.0.0.1:0"
+	c.Coordinator.WriteTimeout = toml.Duration(30 * time.Second)
+
+	c.Meta.Dir = filepath.Join(c.rootPath, "meta")
+
+	c.Data.Dir = filepath.Join(c.rootPath, "data")
+	c.Data.WALDir = filepath.Join(c.rootPath, "wal")
+	c.Data.QueryLogEnabled = verboseServerLogs
+	c.Data.TraceLoggingEnabled = verboseServerLogs
+	c.Data.Index = indexType
+
+	c.HTTPD.Enabled = true
+	c.HTTPD.BindAddress = "127.0.0.1:0"
+	c.HTTPD.LogEnabled = verboseServerLogs
+
+	c.Monitor.StoreEnabled = false
+
+	return c
 }
