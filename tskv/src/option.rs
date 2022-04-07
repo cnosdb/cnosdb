a6e7e45920fbfacbf285f0ff11b564ea6d3f4926
roseboy-liu <roseboy.liu@cnosdb.com>
2022-04-07 18:28:50 +0800

    add option interface from_env

--- tskv/src/option.rs
+++ tskv/src/option.rs
@@ -8,12 +8,36 @@ pub struct Options {
     // pub(crate) write_batch: WriteBatchConfig,
     pub(crate) compact_conf: CompactConfig,
 }
+
+impl Options{
+    //todo: 
+    pub fn from_env() -> Self{
+        Self{
+            .. Default::default()
+        }
+    }
+
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            front_cpu: Default::default(),
+            back_cpu: Default::default(),
+            enable_wal: Default::default(),
+            task_buffer_size: Default::default(),
+            lrucache: Default::default(),
+            compact_conf: Default::default(),
+        }
+    }
+}
 #[allow(dead_code)]
+#[derive(Default)]
 pub struct CacheConfig {}
 
 #[allow(dead_code)]
 pub struct WriteBatchConfig {}
-
+#[derive(Default)]
 pub struct CompactConfig {}
 
 pub struct TimeRange {}
