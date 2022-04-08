c1f7af2d07eebbf5736c47dad44007422cb3a1b7
roseboy-liu <roseboy.liu@cnosdb.com>
2022-04-08 19:05:00 +0800

    feat(verion) add switch memcache

--- tskv/src/option.rs
+++ tskv/src/option.rs
@@ -1,4 +1,7 @@
-#[allow(dead_code)]
+#![allow(dead_code)]
+
+pub const MAX_MEMCACHE_SIZE: u64 = 100 * 1024 * 1024;  //100M 
+
 pub struct Options {
     pub(crate) front_cpu: usize,
     pub(crate) back_cpu: usize,
@@ -9,14 +12,13 @@ pub struct Options {
     pub(crate) compact_conf: CompactConfig,
 }
 
-impl Options{
-    //todo: 
-    pub fn from_env() -> Self{
-        Self{
-            .. Default::default()
+impl Options {
+    //todo:
+    pub fn from_env() -> Self {
+        Self {
+            ..Default::default()
         }
     }
-
 }
 
 impl Default for Options {
@@ -31,21 +33,63 @@ impl Default for Options {
         }
     }
 }
-#[allow(dead_code)]
+
 #[derive(Default)]
 pub struct CacheConfig {}
 
-#[allow(dead_code)]
 pub struct WriteBatchConfig {}
 #[derive(Default)]
 pub struct CompactConfig {}
 
 pub struct TimeRange {}
 
-#[allow(dead_code)]
 pub struct QueryOption {
     timerange: TimeRange,
     db: String,
     table: String,
     series_id: u64,
+}
+
+#[derive(Clone, PartialEq)]
+pub struct TseriesFamOpt {
+    max_level: u32,
+    max_bytes_for_level_base: u64,
+    level_ratio: f64,
+    target_file_size_base: u64,
+    file_number_compact_trigger: u32,
+    max_compact_size: u64,
+}
+
+impl Default for TseriesFamOpt {
+    fn default() -> Self {
+        Self {
+            max_level: 6,
+            max_bytes_for_level_base: 256 * 1024 * 1024,
+            level_ratio: 16f64,
+            target_file_size_base: 64 * 1024 * 1024,
+            file_number_compact_trigger: 4,
+            max_compact_size: 2 * 1024 * 1024 * 1024,
+        }
+    }
+}
+
+pub struct TseriesFamDesc {
+    pub name: String,
+    pub opt: TseriesFamOpt,
+}
+
+pub struct MemCacheOpt {
+    tf_id: u32,
+    max_size: u64,
+    seq_no: u64,
+}
+
+impl Default for MemCacheOpt {
+    fn default() -> Self {
+        Self {
+            tf_id: 0,
+            max_size: 100 * 1024 * 1024,
+            seq_no: 0,
+        }
+    }
 }
