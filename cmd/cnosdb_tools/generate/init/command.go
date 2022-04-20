eca446df5904dd615132a86750ef3f6f94b2cea1
oyzg <2317437823@qq.com>
2022-04-20 14:28:44 +0800

    fix(c1-15:cmd):repair cnosdb_tools generate init

--- cmd/cnosdb_tools/generate/init/command.go
+++ cmd/cnosdb_tools/generate/init/command.go
@@ -3,12 +3,12 @@ package init
 //
 import (
 	"errors"
-	"flag"
 	"github.com/cnosdb/cnosdb/cmd/cnosdb_tools/generate"
 	"github.com/cnosdb/cnosdb/cmd/cnosdb_tools/server"
 	"github.com/spf13/cobra"
 	"io"
 	"os"
+	"time"
 )
 
 // Options represents the program execution for "store query".
@@ -41,21 +41,34 @@ func GetCommand() *cobra.Command {
 		Short: "creates database and retention policy metadata",
 
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if err := run(opt, args); err != nil {
-				return err
-			}
-			return nil
+			//fmt.Println(opt.configPath)
+			//var path string
+			//if c := cmd.Flag("config"); c != nil {
+			//	path = c.Value.String()
+			//}
+			//
+			//if path != "" {
+			//	fmt.Fprintf(os.Stderr, "Merging with configuration at: %s\n", path)
+			//	opt.configPath = path
+			//}
+
+			return run(opt)
 		},
 	}
+	c.PersistentFlags().StringVar(&opt.configPath, "config", "", "Config file")
+	c.PersistentFlags().BoolVar(&opt.printOnly, "print", false, "Print data spec only")
+
+	c.PersistentFlags().StringVar(&opt.spec.StartTime, "start-time", "", "Start time")
+	c.PersistentFlags().StringVar(&opt.spec.Database, "db", "db", "Name of database to create")
+	c.PersistentFlags().StringVar(&opt.spec.Retention, "rp", "rp", "Name of retention policy")
+	c.PersistentFlags().IntVar(&opt.spec.ReplicaN, "rf", 1, "Replication factor")
+	c.PersistentFlags().IntVar(&opt.spec.ShardCount, "shards", 1, "Number of shards to create")
+	c.PersistentFlags().DurationVar(&opt.spec.ShardDuration, "shard-duration", 24*time.Hour, "Shard duration (default 24h)")
 	return c
 }
 
 //
-func run(opt *Options, args []string) (err error) {
-	err = parseFlags(args)
-	if err != nil {
-		return err
-	}
+func run(opt *Options) (err error) {
 
 	err = opt.server.Open(opt.configPath)
 	if err != nil {
@@ -73,18 +86,15 @@ func run(opt *Options, args []string) (err error) {
 		return plan.InitMetadata(opt.server.MetaClient())
 	}
 
+	err = parseFlags()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func parseFlags(args []string) error {
-	fs := flag.NewFlagSet("gen-init", flag.ContinueOnError)
-	fs.StringVar(&opt.configPath, "config", "", "Config file")
-	fs.BoolVar(&opt.printOnly, "print", false, "Print data spec only")
-	opt.spec.AddFlags(fs)
-
-	if err := fs.Parse(args); err != nil {
-		return err
-	}
+func parseFlags() error {
 
 	if opt.spec.Database == "" {
 		return errors.New("database is required")
