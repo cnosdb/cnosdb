409b097bf3404792f5f772478a3e8b38e8763a75
QuenKar <47681251+QuenKar@users.noreply.github.com>
2022-04-22 12:00:12 +0800

    Update server.go

--- cmd/cnosdb-tools/server/server.go
+++ cmd/cnosdb-tools/server/server.go
@@ -1,6 +1,10 @@
 package server
 
 import (
+	"errors"
+	"fmt"
+	mainserver "github.com/cnosdb/cnosdb/server"
+	"os"
 	"time"
 
 	"github.com/cnosdb/cnosdb/meta"
@@ -32,4 +36,98 @@ type MetaClient interface {
 	// that may contain data for the specified time range and limits the Shards to the current node only.
 	// Shard groups are sorted by start time.
 	NodeShardGroupsByTimeRange(database, policy string, min, max time.Time) (a []meta.ShardGroupInfo, err error)
+}
+
+func NewSingleServer() *singleServer {
+	return &singleServer{logger: zap.NewNop()}
+}
+
+type singleServer struct {
+	logger   *zap.Logger
+	config   *mainserver.Config
+	noClient bool
+	client   *meta.Client
+	mc       MetaClient
+}
+
+func (s *singleServer) Open(path string) (err error) {
+	s.config, err = s.parseConfig(path)
+	if err != nil {
+		return err
+	}
+
+	// Validate the configuration.
+	if err = s.config.Validate(); err != nil {
+		return fmt.Errorf("validate config: %s", err)
+	}
+
+	if s.noClient {
+		return nil
+	}
+
+	s.client = meta.NewClient(s.config.Meta)
+	if err = s.client.Open(); err != nil {
+		s.client = nil
+		return err
+	}
+	s.mc = &ossMetaClient{s.client}
+	return nil
+}
+
+func (s *singleServer) Close() {
+	if s.client != nil {
+		s.client.Close()
+		s.client = nil
+	}
+}
+
+func (s *singleServer) MetaClient() MetaClient  { return s.mc }
+func (s *singleServer) TSDBConfig() tsdb.Config { return s.config.Data }
+func (s *singleServer) Logger() *zap.Logger     { return s.logger }
+func (s *singleServer) NodeID() uint64          { return 0 }
+
+// ParseConfig parses the config at path.
+// It returns a demo configuration if path is blank.
+func (s *singleServer) parseConfig(path string) (*mainserver.Config, error) {
+	path = s.resolvePath(path)
+	// Use demo configuration if no config path is specified.
+	if path == "" {
+		return nil, errors.New("missing config file")
+	}
+
+	config := mainserver.NewConfig()
+	if err := config.FromTomlFile(path); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func (s *singleServer) resolvePath(path string) string {
+	if path != "" {
+		if path == os.DevNull {
+			return ""
+		}
+		return path
+	}
+
+	for _, p := range []string{
+		os.ExpandEnv("${HOME}/.cnosdb/cnosdb.conf"),
+		"/etc/cnosdb/cnosdb.conf",
+	} {
+		if _, err := os.Stat(p); err == nil {
+			return p
+		}
+	}
+	return ""
+}
+
+type ossMetaClient struct {
+	*meta.Client
+}
+
+func (*ossMetaClient) NodeID() uint64 { return 0 }
+
+func (c *ossMetaClient) NodeShardGroupsByTimeRange(database, policy string, min, max time.Time) (a []meta.ShardGroupInfo, err error) {
+	return c.ShardGroupsByTimeRange(database, policy, min, max)
 }
