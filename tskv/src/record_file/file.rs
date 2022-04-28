cdd3fec97ac7785ebdcd8eecef7114c290b0f9b7
vrg000 <97928905+vrg000@users.noreply.github.com>
2022-04-28 15:19:00 +0800

    feat(record_file): modify LogFileRequest to RecordFileRequest

--- tskv/src/record_file/file.rs
+++ tskv/src/record_file/file.rs
@@ -2,11 +2,11 @@ use super::*;
 use std::fs;
 use std::path::PathBuf;
 
-pub fn open_file(path: &PathBuf) -> LogFileResult<direct_io::File> {
+pub fn open_file(path: &PathBuf) -> RecordFileResult<direct_io::File> {
     file_manager::get_file_manager()
         .open_file_with(
             path.clone(),
             &fs::OpenOptions::new().read(true).write(true).create(true),
         )
-        .map_err(|err| LogFileError::OpenFile { source: err })
+        .map_err(|err| RecordFileError::OpenFile { source: err })
 }
