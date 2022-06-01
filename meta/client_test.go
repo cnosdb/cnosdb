c504d85d87a23c7fe565ee1f290af0995c3a294a
quenkar <15172118655@163.com>
2022-06-01 09:51:51 +0800

    test(meta): add db cases into client_test.go

--- meta/client_test.go
+++ meta/client_test.go
@@ -126,9 +126,160 @@ func TestMetaClient_CreateDatabaseWithRetentionPolicy(t *testing.T) {
 	}
 }
 
+func TestMetaClient_CreateDatabaseWithRetentionPolicy_Conflict_Fields(t *testing.T) {
+	t.Parallel()
 
+	dir, c := newClient()
+	defer os.RemoveAll(dir)
+	defer c.Close()
 
+	duration := 1 * time.Hour
+	replicaN := 1
+	spec := RetentionPolicySpec{
+		Name:               "rp0",
+		Duration:           &duration,
+		ReplicaN:           &replicaN,
+		ShardGroupDuration: 60 * time.Minute,
+	}
+	if _, err := c.CreateDatabaseWithRetentionPolicy("db0", &spec); err != nil {
+		t.Fatal(err)
+	}
 
+	spec2 := spec
+	spec2.Name = spec.Name + "1"
+	if _, err := c.CreateDatabaseWithRetentionPolicy("db0", &spec2); err != ErrRetentionPolicyConflict {
+		t.Fatalf("got %v, but expected %v", err, ErrRetentionPolicyConflict)
+	}
+
+	spec2 = spec
+	duration2 := *spec.Duration + time.Minute
+	spec2.Duration = &duration2
+	if _, err := c.CreateDatabaseWithRetentionPolicy("db0", &spec2); err != ErrRetentionPolicyConflict {
+		t.Fatalf("got %v, but expected %v", err, ErrRetentionPolicyConflict)
+	}
+
+	spec2 = spec
+	replica2 := *spec.ReplicaN + 1
+	spec2.ReplicaN = &replica2
+	if _, err := c.CreateDatabaseWithRetentionPolicy("db0", &spec2); err != ErrRetentionPolicyConflict {
+		t.Fatalf("got %v, but expected %v", err, ErrRetentionPolicyConflict)
+	}
+
+	spec2 = spec
+	spec2.ShardGroupDuration = spec.ShardGroupDuration + time.Minute
+	if _, err := c.CreateDatabaseWithRetentionPolicy("db0", &spec2); err != ErrRetentionPolicyConflict {
+		t.Fatalf("got %v, but expected %v", err, ErrRetentionPolicyConflict)
+	}
+}
+
+func TestMetaClient_CreateDatabaseWithRetentionPolicy_Conflict_NonDefault(t *testing.T) {
+	t.Parallel()
+
+	d, c := newClient()
+	defer os.RemoveAll(d)
+	defer c.Close()
+
+	duration := 1 * time.Hour
+	replicaN := 1
+	spec := RetentionPolicySpec{
+		Name:               "rp0",
+		Duration:           &duration,
+		ReplicaN:           &replicaN,
+		ShardGroupDuration: 60 * time.Minute,
+	}
+
+	if _, err := c.CreateDatabaseWithRetentionPolicy("db0", &spec); err != nil {
+		t.Fatal(err)
+	}
+
+	spec2 := spec
+	spec2.Name = "rp1"
+	if _, err := c.CreateRetentionPolicy("db0", &spec2, false); err != nil {
+		t.Fatal(err)
+	}
+
+	//notice that makeDefault field is not set,that's error.
+	if _, err := c.CreateDatabaseWithRetentionPolicy("db0", &spec2); err != ErrRetentionPolicyConflict {
+		t.Fatalf("got %v, but expected %v", err, ErrRetentionPolicyConflict)
+	}
+}
+
+func TestMetaClient_Databases(t *testing.T) {
+	t.Parallel()
+
+	dir, c := newClient()
+	defer os.RemoveAll(dir)
+	defer c.Close()
+
+	// Create three databases.
+	db, err := c.CreateDatabase("db0")
+	if err != nil {
+		t.Fatal(err)
+	} else if db == nil {
+		t.Fatal("database not found")
+	} else if db.Name != "db0" {
+		t.Fatalf("db name wrong: %s", db.Name)
+	}
+
+	db, err = c.CreateDatabase("db1")
+	if err != nil {
+		t.Fatal(err)
+	} else if db.Name != "db1" {
+		t.Fatalf("db name wrong: %s", db.Name)
+	}
+
+	db, err = c.CreateDatabase("db2")
+	if err != nil {
+		t.Fatal(err)
+	} else if db.Name != "db2" {
+		t.Fatalf("db name wrong: %s", db.Name)
+	}
+
+	dbs := c.Databases()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(dbs) != 3 {
+		t.Fatalf("expected 2 databases but got %d", len(dbs))
+	} else if dbs[0].Name != "db0" {
+		t.Fatalf("db name wrong: %s", dbs[0].Name)
+	} else if dbs[1].Name != "db1" {
+		t.Fatalf("db name wrong: %s", dbs[1].Name)
+	} else if dbs[2].Name != "db2" {
+		t.Fatalf("db name wrong: %s", dbs[2].Name)
+	}
+}
+
+func TestMetaClient_DropDatabase(t *testing.T) {
+	t.Parallel()
+
+	dir, c := newClient()
+	defer os.RemoveAll(dir)
+	defer c.Close()
+
+	if _, err := c.CreateDatabase("db0"); err != nil {
+		t.Fatal(err)
+	}
+
+	db := c.Database("db0")
+	if db == nil {
+		t.Fatalf("database not found")
+	} else if db.Name != "db0" {
+		t.Fatalf("db name wrong: %s", db.Name)
+	}
+
+	if err := c.DropDatabase("db0"); err != nil {
+		t.Fatal(err)
+	}
+
+	if db = c.Database("db0"); db != nil {
+		t.Fatalf("expected database to not return: %v", db)
+	}
+
+	if err := c.DropDatabase("db foo"); err != nil {
+		t.Fatalf("got %v error, but expected no error", err)
+	}
+}
 
 
 
