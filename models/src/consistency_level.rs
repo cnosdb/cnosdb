f039a32f34bba61108e71629cef6f0651711a997
yongtaoliu <97928896+roseboy-liu@users.noreply.github.com>
2022-08-05 19:01:30 +0800

    add coordinator package and some required struct and traits (#527)

---
+++ models/src/consistency_level.rs
@@ -0,0 +1,10 @@
+pub enum ConsistencyLevel {
+    /// allows for hinted handoff, potentially no write happened yet.
+    Any,
+    /// at least one data node acknowledged a write or read.
+    One,
+    /// a quorum of data nodes to acknowledge a write or read.
+    Quorum,
+    /// requires all data nodes to acknowledge a write or read.
+    All,
+}
