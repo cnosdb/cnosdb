f0c38a7ab80e11e0b4d748bec43b4777766b0466
yukkit <yukkit.zhang@cnosdb.com>
2022-09-08 13:46:46 +0800

    feat(function) #554: Support function BOTTOM and TOP

---
+++ query_server/query/src/extension/expr/expr_utils.rs
@@ -0,0 +1,139 @@
+use datafusion::{
+    logical_plan::{ExprVisitable, ExpressionVisitor, Recursion},
+    prelude::Expr,
+};
+
+use datafusion::error::Result;
+
+use super::selector_function::{BOTTOM, TOPK};
+
+/// Replace 'replace' in 'exprs' with 'with'
+pub fn replace_expr_with(exprs: &[Expr], replace: &Expr, with: &Expr) -> Vec<Expr> {
+    exprs
+        .iter()
+        .map(|e| {
+            if e.eq(replace) {
+                return with.clone();
+            }
+
+            e.clone()
+        })
+        .collect()
+}
+
+/// Collect all deeply nested selector function. They are returned in order of occurrence (depth
+/// first), with duplicates omitted.
+pub fn find_selector_function_exprs(exprs: &[Expr]) -> Vec<Expr> {
+    find_exprs_in_exprs(exprs, &|nested_expr| {
+        matches!(
+            nested_expr,
+            Expr::ScalarUDF {
+                fun,
+                ..
+            } if fun.name.eq_ignore_ascii_case(BOTTOM)
+            || fun.name.eq_ignore_ascii_case(TOPK)
+        )
+    })
+}
+
+/// Search the provided `Expr`'s, not has their nested `Expr`
+pub fn find_exprs_in_exprs<F>(exprs: &[Expr], test_fn: &F) -> Vec<Expr>
+where
+    F: Fn(&Expr) -> bool,
+{
+    exprs
+        .iter()
+        .filter(|e| test_fn(e))
+        .fold(vec![], |mut acc, expr| {
+            if !acc.contains(expr) {
+                acc.push(expr.clone())
+            }
+            acc
+        })
+}
+
+/// Collect all deeply nested selector function. They are returned in order of occurrence (depth
+/// first), with duplicates omitted.
+pub fn find_selector_function_exprs_deeply_nested(exprs: &[Expr]) -> Vec<Expr> {
+    find_exprs_in_exprs(exprs, &|nested_expr| {
+        matches!(
+            nested_expr,
+            Expr::ScalarUDF {
+                fun,
+                ..
+            } if fun.name.eq_ignore_ascii_case(BOTTOM)
+            || fun.name.eq_ignore_ascii_case(TOPK)
+        )
+    })
+}
+
+/// Search the provided `Expr`'s, and all of their nested `Expr`, for any that
+/// pass the provided test. The returned `Expr`'s are deduplicated and returned
+/// in order of appearance (depth first).
+pub fn find_exprs_in_exprs_deeply_nested<F>(exprs: &[Expr], test_fn: &F) -> Vec<Expr>
+where
+    F: Fn(&Expr) -> bool,
+{
+    exprs
+        .iter()
+        .flat_map(|expr| find_exprs_in_expr(expr, test_fn))
+        .fold(vec![], |mut acc, expr| {
+            if !acc.contains(&expr) {
+                acc.push(expr)
+            }
+            acc
+        })
+}
+
+/// Search an `Expr`, and all of its nested `Expr`'s, for any that pass the
+/// provided test. The returned `Expr`'s are deduplicated and returned in order
+/// of appearance (depth first).
+fn find_exprs_in_expr<F>(expr: &Expr, test_fn: &F) -> Vec<Expr>
+where
+    F: Fn(&Expr) -> bool,
+{
+    let Finder { exprs, .. } = expr
+        .accept(Finder::new(test_fn))
+        // pre_visit always returns OK, so this will always too
+        .expect("no way to return error during recursion");
+    exprs
+}
+
+// Visitor that find expressions that match a particular predicate
+struct Finder<'a, F>
+where
+    F: Fn(&Expr) -> bool,
+{
+    test_fn: &'a F,
+    exprs: Vec<Expr>,
+}
+
+impl<'a, F> Finder<'a, F>
+where
+    F: Fn(&Expr) -> bool,
+{
+    /// Create a new finder with the `test_fn`
+    fn new(test_fn: &'a F) -> Self {
+        Self {
+            test_fn,
+            exprs: Vec::new(),
+        }
+    }
+}
+
+impl<'a, F> ExpressionVisitor for Finder<'a, F>
+where
+    F: Fn(&Expr) -> bool,
+{
+    fn pre_visit(mut self, expr: &Expr) -> Result<Recursion<Self>> {
+        if (self.test_fn)(expr) {
+            if !(self.exprs.contains(expr)) {
+                self.exprs.push(expr.clone())
+            }
+            // stop recursing down this expr once we find a match
+            return Ok(Recursion::Stop(self));
+        }
+
+        Ok(Recursion::Continue(self))
+    }
+}
---
+++ query_server/query/src/extension/expr/selector_function/bottom.rs
@@ -0,0 +1,48 @@
+use std::sync::Arc;
+
+use datafusion::{
+    arrow::{array::ArrayRef, datatypes::DataType},
+    error::DataFusionError,
+    logical_expr::{
+        aggregate_function::{DATES, NUMERICS, STRINGS, TIMESTAMPS},
+        ReturnTypeFunction, ScalarUDF, Signature, TypeSignature, Volatility,
+    },
+    physical_expr::functions::make_scalar_function,
+};
+
+use spi::query::function::{FunctionMetadataManager, Result};
+
+use super::BOTTOM;
+
+pub fn register_udf(func_manager: &mut dyn FunctionMetadataManager) -> Result<ScalarUDF> {
+    let udf = new();
+    func_manager.register_udf(udf.clone())?;
+    Ok(udf)
+}
+
+fn new() -> ScalarUDF {
+    let func = |_: &[ArrayRef]| {
+        Err(DataFusionError::Execution(format!(
+            "{} has no specific implementation, should be converted to topk operator.",
+            BOTTOM
+        )))
+    };
+    let func = make_scalar_function(func);
+
+    // Accept any numeric value paired with a float64 percentile
+    let type_signatures = STRINGS
+        .iter()
+        .chain(NUMERICS.iter())
+        .chain(TIMESTAMPS.iter())
+        .chain(DATES.iter())
+        // .chain(TIMES.iter())
+        .map(|t| TypeSignature::Exact(vec![t.clone(), DataType::Int64]))
+        .collect();
+
+    let signature = Signature::one_of(type_signatures, Volatility::Immutable);
+
+    let return_type: ReturnTypeFunction =
+        Arc::new(move |input_expr_types| Ok(Arc::new(input_expr_types[0].clone())));
+
+    ScalarUDF::new(BOTTOM, &signature, &return_type, &func)
+}
