b680e5ed4e642d03f6d327b85fed0c841245e8cf
“xmh1011” <1197843839@qq.com>
2022-09-27 20:56:37 +0800

    CD/CI(jenkins):add automated Dockerfile and scripts 1.1

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,33 +2,34 @@
 set -e
 
 if [ "$1" = 'Ubuntu' ]; then
-	exec apt update \
-       apt install --yes pkg-config openssl libssl-dev g++ cmake git \
-       git clone https://github.com/google/flatbuffers.git \
-       cd flatbuffers \
-       cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release \
-       make install \
-       cargo build
+	 apt update \
+   && apt install --yes pkg-config openssl libssl-dev g++ cmake git \
+   && git clone https://github.com/google/flatbuffers.git \
+   && cd flatbuffers \
+   && cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release \
+   && make install \
+   && cargo build
 fi
 
 if [ "$1" = 'Debian' ]; then
-	exec apt update \
-       apt install --yes pkg-config openssl libssl-dev g++ cmake git \
-       git clone https://github.com/google/flatbuffers.git \
-       cd flatbuffers \
-       cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release \
-       make install \
-       cargo build
+	 apt update \
+   && apt install --yes pkg-config openssl libssl-dev g++ cmake git \
+   && git clone https://github.com/google/flatbuffers.git \
+   && cd flatbuffers \
+   && cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release \
+   && make install \
+   && cargo build
 fi
 
 if [ "$1" = 'Archlinux' ]; then
-	exec pacman -Syu \
-       pacman -S --yes pkg-config openssl libssl-dev g++ cmake git flatbuffers \
-       cargo build
+	pacman -Syu \
+  && pacman -S --yes pkg-config openssl libssl-dev g++ cmake git flatbuffers \
+  && cargo build
 fi
 
 if [ "$1" = 'macOS' ]; then
-	exec brew update \
-       brew install --yes pkg-config openssl libssl-dev g++ cmake git flatbuffers \
-       cargo build
+	brew update \
+	&& mkdir /Users/xiaominghao/eldhewfdug \
+  && brew install pkg-config openssl libssl-dev g++ cmake git flatbuffers \
+  && cargo build
 fi
