fea2b1b520de7340836aa80cd4e34e4a58c03a68
Benxiaohai001 <1447333108@qq.com>
2023-04-25 11:03:11 +0800

    add 11 tcs

--- meta/tests/cluster/main.rs
+++ meta/tests/cluster/main.rs
@@ -1,9 +1,18 @@
 // mod test_cluster;
 
 use std::sync::Arc;
+use std::{thread, time};
+// use std::time;
+use meta::{store::command, client};
+use sysinfo::{System, SystemExt, ProcessExt};
+use std::process::Command;
+// use meta::store::command::*;
+// use meta::client::*;
 
+use models::{meta_data::NodeInfo, schema::Tenant};
 use meta::store::Store;
 use openraft::Config;
+use std::env;
 
 pub async fn new_async() -> Arc<Store> {
     let db_path = format!("{}/{}-{}.binlog", "./meta/journal", "test", "1");
@@ -13,12 +22,433 @@ pub async fn new_async() -> Arc<Store> {
     Arc::new(Store::new(db))
 }
 
-// #[test]
-// pub fn test_store(){
-//     // Suite::test_all(new_async)?;
-//     let db = sled::open("/Users/liuyongtao/work/cnosdb/meta/journal1/meta_node-1.binlog").unwrap();
-//     let tree = db.open_tree("state_machine").unwrap();
-//     let res = tree.get(b"last_applied_log").unwrap().unwrap();
-//     let t = serde_json::from_slice::<LogId<ClusterNodeId>>(&res).unwrap();
-//     println!("{}", t);
+#[cfg(test)]
+mod tests {
+    // use std::{process::Output, thread};
+
+    // use meta::model::TenantManager;
+
+    use super::*;
+    #[tokio::test]
+    async fn test_backup() {
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+        let op = star_meta_cluster();
+        assert_eq!(op.status.code(), std::option::Option::Some(0));
+        write_data_to_meta().await;
+        let output = backup();
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        assert_eq!(stdout.contains("127.0.0.1:8888"), true);
+        assert_eq!(stdout.contains("test_add_tenant001"), true);
+        print!("output: {:?}", output.stdout); 
+        kill_cnosdb_meta_process(&"cnosdb-meta");      
+    }
+
+    #[tokio::test]
+    async fn test_restore() {
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+        let op = star_meta_cluster();
+        assert_eq!(op.status.code(), std::option::Option::Some(0));
+        write_data_to_meta().await;
+        let _output = Command::new("curl")
+            .args(["http://127.0.0.1:8901/dump", "-o" , "/tmp/backup.json"])
+            .output()
+            .expect("failed to execute process");
+            drop_data_from_meta().await;
+        let output = check_meta_info("8901".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        assert_ne!(stdout.contains("test_add_tenant001"), true);
+        let output = meta_restore();
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let output = check_meta_info("8901".to_string());
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        print!("output: {:?}", stdout);
+        assert_eq!(stdout.contains("test_add_tenant001"), true);
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+        // let output = backup();
+        // assert_eq!(output.status.code(), std::option::Option::Some(0));
+        // let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        // assert_eq!(stdout.contains("
+    }
+
+    #[tokio::test]
+    async fn test_node_replace() {
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+        let op = star_meta_cluster();
+        assert_eq!(op.status.code(), std::option::Option::Some(0));
+        let output = kill_from_part_str("config_8921.toml".to_string());
+        // println!("pid: {:?}", pid);
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let output = check_meta_info_metrics("8901".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        assert_eq!(stdout.contains("127.0.0.1:8921"), true);
+        let res =  env::current_dir().unwrap();
+        let res_str: String = res.into_os_string().into_string().unwrap();
+        let path = res_str.clone() + "/start_meta.sh";
+        // println!("path: {}", path);
+        let output = Command::new("bash")
+            .arg(path.clone())
+            .output()
+            .expect("failed to execute process");
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        // println!("output: {:?}", output.stdout);
+        let output = check_meta_info_metrics("8901".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        assert_eq!(stdout.contains("127.0.0.1:8931"), true);
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+    }
+
+    #[tokio::test]
+    async fn test_add_meta_node() {
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+        // start meta cluster
+        let op = star_meta_cluster();
+        assert_eq!(op.status.code(), std::option::Option::Some(0));
+        // get current dir
+        let res =  env::current_dir().unwrap();
+        let res_str: String = res.into_os_string().into_string().unwrap();
+        let path = res_str.clone() + "/start_meta.sh";
+        println!("path: {}", path);
+        let output = Command::new("bash")
+            .args([path.clone(), std::string::String::from("add")])
+            .output()
+            .expect("failed to execute process");
+        println!("output: {:?}", output);
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        // get meta cluster info 
+        let output = check_meta_info_metrics("8921".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        println!("output: {:?}", stdout);
+        assert_eq!(stdout.contains("127.0.0.1:8931"), true);
+        assert_eq!(stdout.contains("127.0.0.1:8921"), true);
+        assert_eq!(stdout.contains("127.0.0.1:8911"), true);
+        assert_eq!(stdout.contains("127.0.0.1:8901"), true);
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+
+    }
+
+    #[tokio::test]
+    async fn test_delete_meta_node() {
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+        // start meta cluster
+        let op = star_meta_cluster();
+        assert_eq!(op.status.code(), std::option::Option::Some(0));
+        let output = kill_from_part_str("config_8921.toml".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        // get current dir
+        let res =  env::current_dir().unwrap();
+        let res_str: String = res.into_os_string().into_string().unwrap();
+        let path = res_str.clone() + "/start_meta.sh";
+        println!("path: {}", path);
+        let output = Command::new("bash")
+            .args([path.clone(), std::string::String::from("delete")])
+            .output()
+            .expect("failed to execute process");
+        println!("output: {:?}", output);
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        // get meta cluster info
+        let output = check_meta_info_metrics("8901".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        println!("output: {:?}", stdout);
+        assert_ne!(stdout.contains("127.0.0.1:8921"), true);
+        assert_eq!(stdout.contains("127.0.0.1:8911"), true);
+        assert_eq!(stdout.contains("127.0.0.1:8901"), true);
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+
+    }
+
+    #[tokio::test]
+    async fn test_cluster_leader_election(){
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+        // start meta cluster
+        let op = star_meta_cluster();
+        assert_eq!(op.status.code(), std::option::Option::Some(0));
+        thread::sleep(time::Duration::from_secs(3));
+        let output = check_meta_info_metrics("8901".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        assert_eq!(stdout.contains("Leader"), true);
+        assert_eq!(stdout.contains("127.0.0.1:8901"), true);
+        assert_eq!(stdout.contains("127.0.0.1:8911"), true);
+        assert_eq!(stdout.contains("127.0.0.1:8921"), true);
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+    }
+
+    #[tokio::test]
+    async fn test_verify_log_replica() {
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+        // start meta cluster
+        let op = star_meta_cluster();
+        assert_eq!(op.status.code(), std::option::Option::Some(0));
+        write_data_to_meta().await;
+        // sleep 3 seconds
+        thread::sleep(time::Duration::from_secs(3));
+        // check fist meta node log
+        let output = check_meta_info("8901".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        assert_eq!(stdout.contains("127.0.0.1:8888"), true);
+        assert_eq!(stdout.contains("test_add_tenant001"), true);
+        // check second meta node log
+        let output = check_meta_info("8911".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        assert_eq!(stdout.contains("127.0.0.1:8888"), true);
+        assert_eq!(stdout.contains("test_add_tenant001"), true);
+        // check third meta node log
+        let output = check_meta_info("8921".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        assert_eq!(stdout.contains("127.0.0.1:8888"), true);
+        assert_eq!(stdout.contains("test_add_tenant001"), true);
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+
+    }
+
+    #[tokio::test]
+    async fn test_reselect_new_leader() {
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+        // start meta cluster
+        let op = star_meta_cluster();
+        assert_eq!(op.status.code(), std::option::Option::Some(0));
+        // write data to meta
+        // write_data_to_meta().await;
+        let output = kill_from_part_str("config_8901.toml".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        // sleep 3 seconds
+        thread::sleep(time::Duration::from_secs(10));   
+        // check first meta node log
+        let output = check_meta_info_metrics("8901".to_string());
+        assert_ne!(output.status.code(), std::option::Option::Some(0));
+        let output = check_meta_info_metrics("8911".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout_node2 = String::from_utf8(output.stdout.clone()).unwrap();
+        let output = check_meta_info_metrics("8921".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout_node3 = String::from_utf8(output.stdout.clone()).unwrap();
+        let stdout = stdout_node2 + &stdout_node3;
+        println!("stdout: {}", stdout);
+        assert_eq!(stdout.contains("Leader"), true);
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+    }
+
+    #[tokio::test]
+    async fn test_cluster_scaling_log_replication() {
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+        // start meta cluster
+        let op = star_meta_cluster();
+        assert_eq!(op.status.code(), std::option::Option::Some(0));
+        // write data to meta
+        write_data_to_meta().await;
+        // get current dir
+        let res =  env::current_dir().unwrap();
+        let res_str: String = res.into_os_string().into_string().unwrap();
+        let path = res_str.clone() + "/start_meta.sh";
+        println!("path: {}", path);
+        let output = Command::new("bash")
+            .args([path.clone(), std::string::String::from("add")])
+            .output()
+            .expect("failed to execute process");
+        println!("output: {:?}", output);
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        // sleep 3 seconds
+        thread::sleep(time::Duration::from_secs(3));
+        // check first meta node log
+        let output = check_meta_info("8931".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        assert_eq!(stdout.contains("127.0.0.1:8888"), true);
+        assert_eq!(stdout.contains("test_add_tenant001"), true);
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+    }
+
+    #[tokio::test]
+    async fn test_repeat_request_log_replica() {
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+        // start meta cluster
+        let op = star_meta_cluster();
+        assert_eq!(op.status.code(), std::option::Option::Some(0));
+        // write data to meta
+        write_data_to_meta().await;
+        // first ckeck meta log
+        let output = check_meta_info("8901".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        assert_eq!(stdout.contains("127.0.0.1:8888"), true);
+        assert_eq!(stdout.contains("test_add_tenant001"), true);
+        // sleep 3 seconds
+        thread::sleep(time::Duration::from_secs(3));
+        // second ckeck meta log
+        let output = check_meta_info("8901".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        assert_eq!(stdout.contains("127.0.0.1:8888"), true);
+        assert_eq!(stdout.contains("test_add_tenant001"), true);
+        // sleep 3 seconds
+        thread::sleep(time::Duration::from_secs(3));
+        // third ckeck meta log
+        let output = check_meta_info("8901".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        let stdout = String::from_utf8(output.stdout.clone()).unwrap();
+        assert_eq!(stdout.contains("127.0.0.1:8888"), true);
+        assert_eq!(stdout.contains("test_add_tenant001"), true);
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+    }
+
+    #[tokio::test]
+    async fn test_majority_failure() {
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+        // start meta cluster
+        let op = star_meta_cluster();
+        assert_eq!(op.status.code(), std::option::Option::Some(0));
+        // write data to meta
+        write_data_to_meta().await;
+        // kill meta node 1
+        let output = kill_from_part_str("config_8901.toml".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        // kill meta node 2
+        let output = kill_from_part_str("config_8911.toml".to_string());
+        assert_eq!(output.status.code(), std::option::Option::Some(0));
+        // check meta node 3
+        let output = check_meta_info_metrics("port_8921".to_string());
+        assert_ne!(output.status.code(), std::option::Option::Some(0));
+        // clean env
+        kill_cnosdb_meta_process(&"cnosdb-meta");
+
+    }
+}
+// fn main() {
+//     println!("Hello, world!");
+//     let pid = get_meta_node_pid();
+//     println!("pid: {:?}", pid);
 // }
+
+fn kill_cnosdb_meta_process(process_name: &str) {
+    let system = System::new_all();
+    let process_name = process_name;
+    for (pid, process) in system.processes() {
+        if process.name() == process_name {
+            println!("{}: {}", pid, process.name());
+            let output = Command::new("kill")
+                .args(["-9", &(pid.to_string())])
+                .output()
+                .expect("failed to execute process");
+            println!("status: {}", output.status);
+            
+        }
+    }
+}
+
+fn star_meta_cluster() -> std::process::Output {
+    let res =  env::current_dir().unwrap();
+    let res_str: String = res.into_os_string().into_string().unwrap();
+    let path = res_str.clone() + "/cluster.sh";
+    // println!("path: {}", path);
+    let output = Command::new("bash").arg(path.clone()).output().expect("failed to execute process");
+    output
+}
+
+async fn write_data_to_meta() {
+    let node = NodeInfo {
+        id: 111,
+        disk_free: 1000,
+        is_cold: false,
+        grpc_addr: "".to_string(),
+        http_addr: "127.0.0.1:8888".to_string(),
+        status: 0,
+    };
+    let req = command::WriteCommand::AddDataNode("cluster_xxx".to_string(), node);
+    let cli = client::MetaHttpClient::new("127.0.0.1:8901".to_string());
+    let rsp = cli.write::<command::StatusResponse>(&req).await.unwrap();
+    println!("=== add nodeinfo : {:?}", rsp);
+    // let req = command::WriteCommand::CreateTenant(“cluster_xxx”.to_string(), (), ())
+    let req = command::WriteCommand::CreateTenant("cluster_xxx".to_string(), "test_add_tenant001".to_string(), models::schema::TenantOptions::default());
+    let cli = client::MetaHttpClient::new("127.0.0.1:8901".to_string());
+    let rsp = cli.write::<command::CommonResp<Tenant>>(&req).await.unwrap();
+    println!("=== add tenant: {:?}", rsp);
+
+}
+async fn drop_data_from_meta() {
+    let req = command::WriteCommand::DropTenant("cluster_xxx".to_string(), "test_add_tenant001".to_string());
+    let cli = client::MetaHttpClient::new("127.0.0.1:8901".to_string());
+    let rsp = cli.write::<command::CommonResp<bool>>(&req).await.unwrap();
+    println!("=== drop tanant: {:?}", rsp);
+}
+
+
+fn backup () -> std::process::Output{
+    let output = Command::new("curl")
+        .args(["http://127.0.0.1:8901/dump"])
+        .output()
+        .expect("failed to execute process");
+    println!("status: {}", output.status);
+    output
+}
+
+fn meta_restore() -> std::process::Output {
+    let output = Command::new("curl")
+        .args(["-XPOST", "http://127.0.0.1:8901/restore", "-d", "@/tmp/backup.json"])
+        .output()
+        .expect("failed to execute process");
+    println!("status: {}", output.status);
+    output
+}
+
+fn check_meta_info(port:String) -> std::process::Output {
+    let url = format!("http://127.0.0.1:{}/debug", port); 
+    let output = Command::new("curl")
+        .args([url.as_str()])
+        .output()
+        .expect("failed to execute process");
+    println!("status: {}", output.status);
+    output
+}
+
+fn check_meta_info_metrics(port:String) -> std::process::Output {
+    let url = format!("http://127.0.0.1:{}/metrics", port);
+    let output = Command::new("curl")
+        .args([url.as_str()])
+        .output()
+        .expect("failed to execute process");
+    println!("status: {}", output.status);
+    output
+}
+
+fn kill_from_part_str(part_str:String) -> std::process::Output {
+    // let full_str = format!("ps -ef | grep {} | grep -v grep | awk '{print $2}'", part_str);
+    let full_str:String = "ps -ef | grep ".to_string() + &part_str + "| grep -v grep | awk '{print $2}'";
+    let pro = Command::new("bash")
+        .args(["-c", full_str.as_str()])
+        .output()
+        .expect("failed to execute process");
+    println!("status: {:?}", pro);
+    let pid = String::from_utf8(pro.stdout).unwrap();
+    // let kill = "kill -9 ".to_string() + pid;
+    let kill = "kill -9 ".to_string() + &pid;
+    let pro = Command::new("bash")
+        .args(["-c", &kill])
+        .output()
+        .expect("failed to execute process");
+    println!("status: {:?}", pro);
+    pro
+}
