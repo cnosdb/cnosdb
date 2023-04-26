066ae5e97c8ccbe1db6c5411cba3727e9967a202
yukkit.zhang <yukkit.zhang@cnosdb.com>
2023-04-26 18:02:56 +0800

    feat: add the definition of time_window_gapfill

---
+++ query_server/query/src/extension/expr/scalar_function/gapfill.rs
@@ -0,0 +1,33 @@
+use std::sync::Arc;
+
+use datafusion::arrow::datatypes::{DataType, TimeUnit};
+use datafusion::logical_expr::{
+    function, BuiltinScalarFunction, ReturnTypeFunction, ScalarUDF, Volatility,
+};
+use spi::query::function::FunctionMetadataManager;
+use spi::Result;
+
+use super::{unimplemented_scalar_impl, TIME_WINDOW_GAPFILL};
+
+pub fn register_udf(func_manager: &mut dyn FunctionMetadataManager) -> Result<ScalarUDF> {
+    let udf = new();
+    func_manager.register_udf(udf.clone())?;
+    Ok(udf)
+}
+
+fn new() -> ScalarUDF {
+    // DATE_BIN_GAPFILL should have the same signature as DATE_BIN,
+    // so that just adding _GAPFILL can turn a query into a gap-filling query.
+    let mut signatures = function::signature(&BuiltinScalarFunction::DateBin);
+    // We don't want this to be optimized away before we can give a helpful error message
+    signatures.volatility = Volatility::Volatile;
+
+    let return_type_fn: ReturnTypeFunction =
+        Arc::new(|_| Ok(Arc::new(DataType::Timestamp(TimeUnit::Nanosecond, None))));
+    ScalarUDF::new(
+        TIME_WINDOW_GAPFILL,
+        &signatures,
+        &return_type_fn,
+        &unimplemented_scalar_impl(TIME_WINDOW_GAPFILL),
+    )
+}
