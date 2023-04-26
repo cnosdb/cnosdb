fed5c8d1a7c767149c6982785fb7264b8f30b50e
yukkit.zhang <yukkit.zhang@cnosdb.com>
2023-04-26 18:02:56 +0800

    feat: add the definition of time_window_gapfill

--- query_server/query/src/extension/expr/scalar_function/mod.rs
+++ query_server/query/src/extension/expr/scalar_function/mod.rs
@@ -1,14 +1,30 @@
 #[cfg(test)]
 mod example;
+mod gapfill;
 
+use std::sync::Arc;
+
+use datafusion::error::DataFusionError;
+use datafusion::logical_expr::ScalarFunctionImplementation;
 use spi::query::function::FunctionMetadataManager;
 use spi::Result;
 
-pub fn register_udfs(_func_manager: &mut dyn FunctionMetadataManager) -> Result<()> {
+pub const TIME_WINDOW_GAPFILL: &str = "time_window_gapfill";
+
+pub fn register_udfs(func_manager: &mut dyn FunctionMetadataManager) -> Result<()> {
     // extend function...
     // eg.
     //   example::register_udf(func_manager)?;
+    gapfill::register_udf(func_manager)?;
     Ok(())
+}
+
+fn unimplemented_scalar_impl(name: &'static str) -> ScalarFunctionImplementation {
+    Arc::new(move |_| {
+        Err(DataFusionError::NotImplemented(format!(
+            "{name} is not yet implemented"
+        )))
+    })
 }
 
 #[cfg(test)]
