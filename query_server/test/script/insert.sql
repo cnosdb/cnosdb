ab9edf597827ffbdd0bc507f844185252332d45e
Benxiaohai001 <1447333108@qq.com>
2023-05-05 19:23:40 +0800

    add expansion updata cases

---
+++ query_server/test/script/insert.sql
@@ -0,0 +1,9 @@
+CREATE TABLE air (visibility DOUBLE,temperature DOUBLE,presssure DOUBLE,TAGS(station));
+INSERT INTO air (TIME, station, visibility, temperature, presssure) VALUES (1667456411000000000, 'XiaoMaiDao1', 56, 69, 411);
+INSERT INTO air (TIME, station, visibility, temperature, presssure) VALUES (1667456411000000000, 'XiaoMaiDao2', 56, 69, 411);
+INSERT INTO air (TIME, station, visibility, temperature, presssure) VALUES (1667456411000000000, 'XiaoMaiDao3', 56, 69, 411);
+INSERT INTO air (TIME, station, visibility, temperature, presssure) VALUES (1667456411000000000, 'XiaoMaiDao4', 56, 69, 411);
+INSERT INTO air (TIME, station, visibility, temperature, presssure) VALUES (1667456711000000000, 'XiaoMaiDao1', 56, 69, 711);
+INSERT INTO air (TIME, station, visibility, temperature, presssure) VALUES (1667456711000000000, 'XiaoMaiDao2', 56, 69, 711);
+INSERT INTO air (TIME, station, visibility, temperature, presssure) VALUES (1667456711000000000, 'XiaoMaiDao3', 56, 69, 711);
+INSERT INTO air (TIME, station, visibility, temperature, presssure) VALUES (1667456711000000000, 'XiaoMaiDao4', 56, 69, 711);
