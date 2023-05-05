06d11e837817d128e47973126bb799ada2b603ee
lutengda <18346072982@163.com>
2023-05-05 19:20:49 +0800

    Fixed the problem of ci not passing

--- query_server/test/cases/sys_table/cluster_schema/tenants.sql
+++ query_server/test/cases/sys_table/cluster_schema/tenants.sql
@@ -5,11 +5,11 @@ drop user if exists test_ts_u2;
 create tenant if not exists test_ts_tenant1;
 create tenant test_ts_tenant1 with comment='this is test_ts_tenant1';
 create tenant if not exists test_ts_tenant1 with comment='this is test_ts_tenant1';
-select * from cluster_schema.tenants;
+select * from cluster_schema.tenants where tenant_name in ('test_ts_tenant1');
 alter tenant test_ts_tenant1 set comment='Ig.UZn꓃DH~Bgc.ᵵh7uaV*1lE/}\,J씟\h#f^Kr잿z';
-select * from cluster_schema.tenants;
+select * from cluster_schema.tenants where tenant_name in ('test_ts_tenant1');
 ALTER TENANT test_ts_tenant1 unSET COMMENT;
-select * from cluster_schema.tenants;
+select * from cluster_schema.tenants where tenant_name in ('test_ts_tenant1');
 
 create user if not exists test_ts_u1;
 create user if not exists test_ts_u2;
