0a3db7cd2be6b9d50708ae5e77b7c368dc1571eb
zipper-meng <zipper.meng@cnosdb.com>
2023-06-13 11:41:57 +0800

    fix(ci): docker image built by github actions cannot get the commit hash

--- main/build.rs
+++ main/build.rs
@@ -1,18 +1,38 @@
-// Include the GIT_HASH
-// https://stackoverflow.com/questions/43753491/include-git-commit-hash-as-string-into-rust-program
 use std::process::Command;
-fn main() {
-    let output = Command::new("git").args(["rev-parse", "HEAD"]).output();
 
-    if let Ok(output) = output {
-        if let Ok(git_hash) = String::from_utf8(output.stdout) {
+fn main() {
+    let mut rustc_env_git_hash = false;
+    // Get the GIT_HASH by 'git rev-parse HEAD'.
+    // https://stackoverflow.com/questions/43753491/include-git-commit-hash-as-string-into-rust-program
+    if let Ok(output) = Command::new("git").args(["rev-parse", "HEAD"]).output() {
+        if output.status.success() {
+            if let Ok(git_hash) = String::from_utf8(output.stdout) {
+                println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+                rustc_env_git_hash = true;
+            }
+        } else {
+            eprintln!("Failed to run 'git rev-parse HEAD'");
+        }
+    } else {
+        eprintln!("Failed to start process 'git rev-parse HEAD'");
+    }
+    if !rustc_env_git_hash {
+        // Get the GIT_HASH from environment variable 'CNOSDB_GIT_HASH'
+        // when using Github CI to build.
+        if let Ok(git_hash) = std::env::var("CNOSDB_GIT_HASH") {
             println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+            rustc_env_git_hash = true;
         }
     }
-    let res = Command::new("sh").arg("-c").arg("pwd").output();
-    if let Ok(output) = res {
+    if !rustc_env_git_hash {
+        eprintln!(
+            "Failed to get commit hash by 'git rev-parse HEAD' or from env 'CNOSDB_GIT_HASH'"
+        );
+    }
+
+    if let Ok(output) = Command::new("sh").arg("-c").arg("pwd").output() {
         if let Ok(pwd) = String::from_utf8(output.stdout) {
-            println!("pwd：{}", pwd);
+            println!("pwd: {}", pwd);
         }
     }
 
