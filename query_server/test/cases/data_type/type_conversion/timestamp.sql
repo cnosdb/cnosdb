12145e06e3ba9d696ac73860604b6db2c84f71e7
Kree0 <l.l0228@qq.com>
2023-08-25 16:11:20 +0800

    improve: enable shell scripts to be added to test cases

--- query_server/test/cases/data_type/type_conversion/timestamp.sql
+++ query_server/test/cases/data_type/type_conversion/timestamp.sql
@@ -1,6 +1,8 @@
 -- normal
 --#SLEEP=100
 -- public.test(time i64, ta utf8, tb utf8, fa , fb)
+drop table if exists test_timestamp_conv;
+alter database public set ttl '1000000d';
 --#LP_BEGIN
 test_timestamp_conv,ta=a1,tb=b1 fa=1,fb=2 3
 --#LP_END
