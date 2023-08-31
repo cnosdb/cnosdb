75fc369bd6b0ef5f4180e130de28355ede695a4e
ZuoTiJia <changchengxing@gmail.com>
2023-08-31 10:53:48 +0800

    improve: impl Ord for DataType
    * refactor: improve TimeRanges method `overlap`
    
    * open debug_assert for profile.test-ci

--- tskv/src/reader/iterator.rs
+++ tskv/src/reader/iterator.rs
@@ -15,8 +15,7 @@ use minivec::MiniVec;
 use models::meta_data::VnodeId;
 use models::predicate::domain::{self, QueryArgs, QueryExpr, TimeRanges};
 use models::predicate::PlacedSplit;
-use models::schema::{PhysicalCType as ColumnType, TableColumn, TskvTableSchema};
-use models::schema::{TskvTableSchemaRef};
+use models::schema::{PhysicalCType as ColumnType, TableColumn, TskvTableSchemaRef};
 use models::utils::{min_num, unite_id};
 use models::{FieldId, PhysicalDType as ValueType, SeriesId, Timestamp};
 use protos::kv_service::QueryRecordBatchRequest;
@@ -554,12 +553,15 @@ impl Cursor for TimeCursor {
 //-----------Tag Cursor----------------
 pub struct TagCursor {
     name: String,
-    value: Option<Vec<u8>>,
+    value: Option<DataType>,
 }
 
 impl TagCursor {
     pub fn new(name: String, value: Option<Vec<u8>>) -> Self {
-        Self { name, value }
+        Self {
+            name,
+            value: value.map(|v| DataType::Str(0, MiniVec::from(v.as_slice()))),
+        }
     }
 }
 
@@ -576,16 +578,13 @@ impl Cursor for TagCursor {
     async fn next(&mut self, _ts: i64) {}
 
     async fn peek(&mut self) -> Result<Option<DataType>> {
-        match &self.value {
-            Some(value) => Ok(Some(DataType::Str(0, MiniVec::from(value.as_slice())))),
-            None => Ok(None),
-        }
+        Ok(self.value.clone())
     }
 }
 
 //-----------Field Cursor----------------
 pub struct FieldCursor {
-    name: String,
+    name: Arc<String>,
     value_type: ValueType,
 
     cache_index: usize,
@@ -594,7 +593,7 @@ pub struct FieldCursor {
 }
 
 impl FieldCursor {
-    pub fn empty(value_type: ValueType, name: String) -> Self {
+    pub fn empty(value_type: ValueType, name: Arc<String>) -> Self {
         Self {
             name,
             value_type,
@@ -1134,7 +1133,7 @@ impl SeriesGroupRowIterator {
                             let cursor = self
                                 .build_field_cursor(
                                     unite_id(item.id, series_id),
-                                    item.name.clone(),
+                                    Arc::new(item.name.clone()),
                                     vtype,
                                 )
                                 .await?;
@@ -1159,7 +1158,7 @@ impl SeriesGroupRowIterator {
     async fn build_field_cursor(
         &self,
         field_id: FieldId,
-        field_name: String,
+        field_name: Arc<String>,
         field_type: ValueType,
     ) -> Result<FieldCursor> {
         let super_version = match self.super_version {
@@ -1244,7 +1243,7 @@ impl SeriesGroupRowIterator {
         })
     }
 
-    async fn collect_row_data(&mut self, builder: &mut [ArrayBuilderPtr]) -> Result<Option<()>> {
+    async fn collect_row_data(&mut self, builders: &mut [ArrayBuilderPtr]) -> Result<Option<()>> {
         trace::trace!("======collect_row_data=========");
         // Record elapsed_field_scan
         let timer = self.metrics.elapsed_field_scan().timer();
@@ -1301,13 +1300,13 @@ impl SeriesGroupRowIterator {
         for (i, value) in row_cols.into_iter().enumerate() {
             match self.columns[i].column_type() {
                 ColumnType::Time(unit) => {
-                    builder[i].append_timestamp(&unit, min_time);
+                    builders[i].append_timestamp(&unit, min_time);
                 }
                 ColumnType::Tag => {
-                    builder[i].append_value(ValueType::String, value, self.columns[i].name())?;
+                    builders[i].append_value(ValueType::String, value, self.columns[i].name())?;
                 }
                 ColumnType::Field(value_type) => {
-                    builder[i].append_value(value_type, value, self.columns[i].name())?;
+                    builders[i].append_value(value_type, value, self.columns[i].name())?;
                 }
             }
         }
