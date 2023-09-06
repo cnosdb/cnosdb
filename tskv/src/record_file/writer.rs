5df196e0296ad81c2ede79d9929bb93670427b78
zipper-meng <zipper.meng@cnosdb.com>
2023-09-06 14:52:20 +0800

    feat(raft): read&writer for current wal use the same file descriptor

--- tskv/src/record_file/writer.rs
+++ tskv/src/record_file/writer.rs
@@ -1,40 +1,47 @@
-use std::io::{IoSlice, SeekFrom};
+use std::io::IoSlice;
 use std::path::{Path, PathBuf};
+use std::sync::Arc;
 
 use num_traits::ToPrimitive;
 use snafu::ResultExt;
 
 use super::{
-    file_crc_source_len, Reader, RecordDataType, FILE_FOOTER_LEN, FILE_MAGIC_NUMBER,
+    file_crc_source_len, reader, RecordDataType, FILE_FOOTER_LEN, FILE_MAGIC_NUMBER,
     FILE_MAGIC_NUMBER_LEN, RECORD_MAGIC_NUMBER,
 };
 use crate::error::{self, Error, Result};
-use crate::file_system::file::cursor::FileCursor;
+use crate::file_system::file::async_file::AsyncFile;
 use crate::file_system::file::IFile;
 use crate::file_system::file_manager;
 
 pub struct Writer {
     path: PathBuf,
-    cursor: FileCursor,
+    file: Arc<AsyncFile>,
+
     footer: Option<[u8; FILE_FOOTER_LEN]>,
+    pos: u64,
     file_size: u64,
 }
 
 impl Writer {
     pub async fn open(path: impl AsRef<Path>, _data_type: RecordDataType) -> Result<Self> {
         let path = path.as_ref();
-        let mut cursor: FileCursor = file_manager::open_create_file(path).await?.into();
-        let mut file_size = 0_u64;
-        let footer = if cursor.is_empty() {
+        let file = file_manager::open_create_file(path).await?;
+        if file.is_empty() {
             // For new file, write file magic number, next write is at 4.
-            file_size += cursor
-                .write(&FILE_MAGIC_NUMBER.to_be_bytes())
+            let file_size = file
+                .write_at(0, &FILE_MAGIC_NUMBER.to_be_bytes())
                 .await
                 .context(error::IOSnafu)? as u64;
-            // Get none as footer data.
-            None
+            Ok(Writer {
+                path: path.to_path_buf(),
+                file: Arc::new(file),
+                footer: None,
+                pos: file_size,
+                file_size,
+            })
         } else {
-            let footer_data = match Reader::read_footer(&path).await {
+            let footer = match reader::read_footer(&path).await {
                 Ok((_, f)) => Some(f),
                 Err(Error::NoFooter) => None,
                 Err(e) => {
@@ -46,22 +53,24 @@ impl Writer {
                 }
             };
 
-            // For writed file, skip to footer position, next write is at (file.len() - footer_len).
+            // For wrote file, skip to footer position, next write is at (file.len() - footer_len).
             // Note that footer_len may be zero.
-            let seek_pos_end = footer_data.map(|f| f.len()).unwrap_or(0);
-            // TODO: truncate this file using seek_pos_end.
-            cursor
-                .seek(SeekFrom::End(-(seek_pos_end as i64)))
-                .context(error::IOSnafu)?;
+            let footer_len = footer.map(|f| f.len()).unwrap_or(0);
+            // Truncate this file to overwrite footer data.
+            let file_size = file
+                .len()
+                .checked_sub(footer_len as u64)
+                .unwrap_or(FILE_MAGIC_NUMBER_LEN as u64);
+            file.truncate(file_size).await?;
 
-            footer_data
-        };
-        Ok(Writer {
-            path: path.to_path_buf(),
-            cursor,
-            footer,
-            file_size,
-        })
+            Ok(Writer {
+                path: path.to_path_buf(),
+                file: Arc::new(file),
+                footer,
+                pos: file_size,
+                file_size,
+            })
+        }
     }
 
     // Writes record data and returns the written data size.
@@ -112,14 +121,15 @@ impl Writer {
         }
 
         // Write record header and record data.
-        let written_size =
-            self.cursor
-                .write_vec(&mut write_buf)
-                .await
-                .map_err(|e| Error::WriteFile {
-                    path: self.path.clone(),
-                    source: e,
-                })?;
+        let written_size = self
+            .file
+            .write_vec(self.pos, &mut write_buf)
+            .await
+            .map_err(|e| Error::WriteFile {
+                path: self.path.clone(),
+                source: e,
+            })?;
+        self.pos += written_size as u64;
         self.file_size += written_size as u64;
         Ok(written_size)
     }
@@ -129,7 +139,7 @@ impl Writer {
 
         // Get file crc
         let mut buf = vec![0_u8; file_crc_source_len(self.file_size(), 0_usize)];
-        self.cursor
+        self.file
             .read_at(FILE_MAGIC_NUMBER_LEN as u64, &mut buf)
             .await
             .map_err(|e| Error::ReadFile {
@@ -142,33 +152,50 @@ impl Writer {
         footer[4..8].copy_from_slice(&crc.to_be_bytes());
         self.footer = Some(*footer);
 
-        self.cursor
-            .write(footer)
-            .await
-            .map_err(|e| Error::WriteFile {
-                path: self.path.clone(),
-                source: e,
-            })
-    }
-
-    pub fn footer(&self) -> Option<[u8; FILE_FOOTER_LEN]> {
-        self.footer
+        let written_size =
+            self.file
+                .write_at(self.pos, footer)
+                .await
+                .map_err(|e| Error::WriteFile {
+                    path: self.path.clone(),
+                    source: e,
+                })?;
+        // Only add file_size, does not add pos
+        self.file_size += written_size as u64;
+        Ok(written_size)
     }
 
     pub async fn sync(&self) -> Result<()> {
-        self.cursor.sync_data().await.context(error::SyncFileSnafu)
+        self.file.sync_data().await.context(error::SyncFileSnafu)
     }
 
     pub async fn close(&mut self) -> Result<()> {
         self.sync().await
     }
 
-    pub fn file_size(&self) -> u64 {
-        self.file_size
+    pub fn new_reader(&self) -> reader::Reader {
+        reader::Reader::new(
+            self.file.clone(),
+            self.path.clone(),
+            self.file_size,
+            self.footer,
+        )
     }
 
     pub fn path(&self) -> PathBuf {
         self.path.clone()
+    }
+
+    pub fn footer(&self) -> Option<[u8; FILE_FOOTER_LEN]> {
+        self.footer
+    }
+
+    pub fn pos(&self) -> u64 {
+        self.pos
+    }
+
+    pub fn file_size(&self) -> u64 {
+        self.file_size
     }
 }
 
