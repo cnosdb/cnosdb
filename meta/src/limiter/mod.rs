415f9c94968d1b04f69660b38db24754094a41a0
ZuoTiJia <changchengxing@gmail.com>
2023-09-11 16:55:39 +0800

    doc: add limiter description

--- meta/src/limiter/mod.rs
+++ meta/src/limiter/mod.rs
@@ -15,6 +15,31 @@ pub use local_request_limiter::LocalRequestLimiter;
 pub use none_limiter::NoneLimiter;
 
 pub type LimiterRef = Arc<dyn RequestLimiter>;
+
+/// The distributed current limiter is divided into two parts.
+/// There is a cached LocalBucket on the data node,
+/// and a RateBucket on the meta node.
+///
+/// When a request comes,
+/// the token bucket on the data node is first accessed.
+/// If the token bucket in the data cache has enough tokens, the request can pass.
+/// If the token bucket in the data cache is not enough,
+/// the greedy method is used to request tokens from the Meta's RateBucket.
+///
+/// When the current limiter configuration changes,
+/// the local LocalBucket is changed through the watch mechanism.
+// │                                                               x
+// │
+// │                                                               x
+// │                                                     ┌────────────────────┐
+// │                 ┌────────────────┐                  │                    │
+// │  require local  │                │  require remote  │      x     x       │
+// │      6 x        │                ├─────────────────►│         x          │
+// ├────────────────►│  x    x     x  │                  │                    │
+// │                 │                │                  │      x x     x     │
+// │                 └────────────────┘                  │                    │
+// │                                                     └────────────────────┘
+///
 #[async_trait]
 pub trait RequestLimiter: Send + Sync + Debug {
     async fn check_data_in(&self, data_len: usize) -> MetaResult<()>;
