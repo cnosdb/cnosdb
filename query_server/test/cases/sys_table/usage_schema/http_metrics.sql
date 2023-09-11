7adc9f408c770d4e25fa79902da08f6d0ff5dcc9
ZuoTiJia <changchengxing@gmail.com>
2023-09-11 10:25:32 +0800

    feat: add http metrics
    * rename http_metrics usage_schema

---
+++ query_server/test/cases/sys_table/usage_schema/http_metrics.sql
@@ -0,0 +1,11 @@
+--#DATABASE = usage_schema
+--#SLEEP = 100
+DESCRIBE DATABASE usage_schema;
+
+DESCRIBE TABLE http_data_in;
+
+DESCRIBE TABLE http_data_out;
+
+DESCRIBE TABLE http_queries;
+
+DESCRIBE TABLE http_writes;
