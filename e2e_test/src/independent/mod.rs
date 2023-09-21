ac39179fc9b937a5cce9c8af6e177f8bc71ac116
Kree0 <l.l0228@qq.com>
2023-09-21 11:31:29 +0800

    ci: optimize e2e_test structure, add new test cases

---
+++ e2e_test/src/independent/mod.rs
@@ -0,0 +1,8 @@
+#[cfg(feature = "not_passed")]
+mod chaos_tests;
+#[cfg(feature = "coordinator_e2e_test")]
+#[cfg(test)]
+mod coordinator_tests;
+mod https_api_tests;
+#[cfg(feature = "not_passed")]
+mod restart_tests;
