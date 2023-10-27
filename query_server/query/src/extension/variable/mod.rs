0f5f7768d2fedbc06897e166fb8171f121740b9f
yukkit.zhang <yukkit.zhang@cnosdb.com>
2023-10-27 20:48:38 +0800

    feat: add built-in system variable @@server_version

--- query_server/query/src/extension/variable/mod.rs
+++ query_server/query/src/extension/variable/mod.rs
@@ -1,7 +1,10 @@
+mod server_version;
+
 use spi::query::variable::SystemVariableManager;
 use spi::Result;
 
-pub fn load_all_system_vars(_var_manager: &mut dyn SystemVariableManager) -> Result<()> {
-    // TODO: load all system variables
+pub fn load_all_system_vars(var_manager: &mut dyn SystemVariableManager) -> Result<()> {
+    // load all system variables
+    server_version::register_variable(var_manager)?;
     Ok(())
 }
