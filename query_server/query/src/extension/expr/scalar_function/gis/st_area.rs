c3d764519d7b2bcbb9a6af12401d694c200143e1
ZuoTiJia <changchengxing@gmail.com>
2023-11-14 20:08:40 +0800

    refactor: refactor gis function implementation
    * add `geometry_binary_op`
    
    * add `geometry_unary_op`

--- query_server/query/src/extension/expr/scalar_function/gis/st_area.rs
+++ query_server/query/src/extension/expr/scalar_function/gis/st_area.rs
@@ -1,42 +1,20 @@
-use std::sync::Arc;
-
-use datafusion::arrow::array::{downcast_array, ArrayRef, Float64Array, StringArray};
+use datafusion::arrow::array::{downcast_array, ArrayRef, Float64Builder, StringArray};
 use datafusion::arrow::datatypes::DataType;
-use datafusion::logical_expr::{ReturnTypeFunction, ScalarUDF, Signature, Volatility};
+use datafusion::error::DataFusionError;
+use datafusion::logical_expr::ScalarUDF;
 use datafusion::physical_plan::functions::make_scalar_function;
-use geo::Area;
-use geozero::wkt::WktStr;
-use geozero::ToGeo;
+use geo::{Area, Geometry};
 use spi::query::function::FunctionMetadataManager;
-use spi::{DFResult, Result};
+use spi::Result;
+
+use crate::geometry_unary_op;
 
 pub fn register_udf(func_manager: &mut dyn FunctionMetadataManager) -> Result<ScalarUDF> {
-    let udf = new();
+    let udf = geometry_unary_op!("ST_Area", area, DataType::Float64, Float64Builder);
     func_manager.register_udf(udf.clone())?;
     Ok(udf)
 }
 
-fn new() -> ScalarUDF {
-    let fun = make_scalar_function(func);
-
-    let signature = Signature::exact(vec![DataType::Utf8], Volatility::Immutable);
-    let return_type: ReturnTypeFunction = Arc::new(move |_| Ok(Arc::new(DataType::Float64)));
-
-    ScalarUDF::new("st_Area", &signature, &return_type, &fun)
-}
-
-fn func(args: &[ArrayRef]) -> DFResult<ArrayRef> {
-    let geos = args[0].as_ref();
-    let geos = downcast_array::<StringArray>(geos);
-    let areas = geos
-        .iter()
-        .map(|opt| {
-            opt.and_then(|str| {
-                let wkt = WktStr(str);
-                wkt.to_geo().ok().map(|g| g.unsigned_area())
-            })
-        })
-        .collect::<Float64Array>();
-
-    Ok(Arc::new(areas))
+fn area(geo: &Geometry) -> Result<f64, DataFusionError> {
+    Ok(geo.unsigned_area())
 }
