be89d57f59e9799a73716450133661df7d53bcea
Kree0 <l.l0228@qq.com>
2023-12-04 16:13:27 +0800

    feature: add pool for HTTP content codecs

--- common/http_protocol/src/encoding.rs
+++ common/http_protocol/src/encoding.rs
@@ -1,3 +1,4 @@
+use std::cell::RefCell;
 use std::io::{self, Cursor, Write};
 use std::slice::Iter;
 
@@ -35,64 +36,72 @@ pub trait EncodingExt {
     }
 }
 
+thread_local! {
+    pub static BUFFER: RefCell<Vec<u8>> = RefCell::new(Vec::with_capacity(4096));
+}
+
 impl EncodingExt for Encoding {
     fn encode(&self, bytes: Vec<u8>) -> io::Result<Vec<u8>> {
         Ok(match self {
-            Encoding::Gzip => {
-                let mut encoder = GzEncoder::new(vec![], Compression::default());
+            Encoding::Gzip => BUFFER.with(|buf| {
+                let buf = &mut *buf.borrow_mut();
+                buf.clear();
+                let mut encoder = GzEncoder::new(buf, Compression::default());
                 encoder.write_all(&bytes)?;
-                encoder.finish()?
-            }
-            Encoding::Deflate => {
-                let mut encoder = DeflateEncoder::new(vec![], Compression::default());
+                encoder.finish().map(|res| res.clone())
+            })?,
+            Encoding::Deflate => BUFFER.with(|buf| {
+                let buf = &mut *buf.borrow_mut();
+                buf.clear();
+                let mut encoder = DeflateEncoder::new(buf, Compression::default());
                 encoder.write_all(&bytes)?;
-                encoder.finish()?
-            }
-            Encoding::Brotli => {
+                encoder.finish().map(|res| res.clone())
+            })?,
+            Encoding::Brotli => BUFFER.with(|buf| {
+                let buf = &mut *buf.borrow_mut();
+                buf.clear();
                 let params = BrotliEncoderParams::default();
-                let mut compressed_bytes = vec![];
                 let mut cursor = Cursor::new(bytes);
-                brotli::BrotliCompress(&mut cursor, &mut compressed_bytes, &params)
-                    .map(|_| compressed_bytes)?
-            }
-            Encoding::Zstd => {
-                let mut compressed_bytes = vec![];
-                zstd::stream::copy_encode(
-                    &bytes[..],
-                    &mut compressed_bytes,
-                    DEFAULT_COMPRESSION_LEVEL,
-                )?;
-                compressed_bytes
-            }
+                brotli::BrotliCompress(&mut cursor, buf, &params).map(|_| buf.clone())
+            })?,
+            Encoding::Zstd => BUFFER.with(|buf| {
+                let buf = &mut *buf.borrow_mut();
+                buf.clear();
+                zstd::stream::copy_encode(&bytes[..], &mut *buf, DEFAULT_COMPRESSION_LEVEL)?;
+                io::Result::Ok(buf.clone())
+            })?,
             Encoding::Identity => bytes,
         })
     }
 
     fn decode(&self, bytes: Bytes) -> io::Result<Bytes> {
         Ok(match self {
-            Encoding::Gzip => {
-                let mut decoder = GzDecoder::new(vec![]);
+            Encoding::Gzip => BUFFER.with(|buf| {
+                let buf = &mut *buf.borrow_mut();
+                buf.clear();
+                let mut decoder = GzDecoder::new(buf);
                 decoder.write_all(&bytes)?;
-                let decompressed_bytes = decoder.finish()?;
-                Bytes::from(decompressed_bytes)
-            }
-            Encoding::Deflate => {
-                let mut decoder = DeflateDecoder::new(vec![]);
+                decoder.finish().map(|res| Bytes::from(res.clone()))
+            })?,
+            Encoding::Deflate => BUFFER.with(|buf| {
+                let buf = &mut *buf.borrow_mut();
+                buf.clear();
+                let mut decoder = DeflateDecoder::new(buf);
                 decoder.write_all(&bytes)?;
-                let decompressed_bytes = decoder.finish()?;
-                Bytes::from(decompressed_bytes)
-            }
-            Encoding::Brotli => {
-                let mut decompressed_bytes = vec![];
+                decoder.finish().map(|res| Bytes::from(res.clone()))
+            })?,
+            Encoding::Brotli => BUFFER.with(|buf| {
+                let buf = &mut *buf.borrow_mut();
+                buf.clear();
                 let mut cursor = Cursor::new(bytes);
-                brotli::BrotliDecompress(&mut cursor, &mut decompressed_bytes)
-                    .map(|_| Bytes::from(decompressed_bytes))?
-            }
-            Encoding::Zstd => {
-                let mut decompressed_bytes = vec![];
-                zstd::stream::copy_decode(&bytes[..], &mut decompressed_bytes)?;
-                Bytes::from(decompressed_bytes)
-            }
+                brotli::BrotliDecompress(&mut cursor, buf).map(|_| Bytes::from(buf.clone()))
+            })?,
+            Encoding::Zstd => BUFFER.with(|buf| {
+                let buf = &mut *buf.borrow_mut();
+                buf.clear();
+                zstd::stream::copy_decode(&bytes[..], &mut *buf)?;
+                io::Result::Ok(Bytes::from(buf.clone()))
+            })?,
             Encoding::Identity => bytes,
         })
     }
