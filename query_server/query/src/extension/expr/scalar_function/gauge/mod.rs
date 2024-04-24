eda1e2890484f51351deadf28f509e0ab6d2033f
lutengda <18346072982@163.com>
2024-04-24 10:28:54 +0800

    feat: add first_time,first_val,last_time,last_val,idelta_left,idelta_right func for gauge_agg

--- query_server/query/src/extension/expr/scalar_function/gauge/mod.rs
+++ query_server/query/src/extension/expr/scalar_function/gauge/mod.rs
@@ -2,6 +2,12 @@ use spi::query::function::FunctionMetadataManager;
 use spi::Result;
 
 mod delta;
+mod first_time;
+mod first_val;
+mod idelta_left;
+mod idelta_right;
+mod last_time;
+mod last_val;
 mod rate;
 mod time_delta;
 
@@ -9,5 +15,11 @@ pub fn register_udfs(func_manager: &mut dyn FunctionMetadataManager) -> Result<(
     delta::register_udf(func_manager)?;
     time_delta::register_udf(func_manager)?;
     rate::register_udf(func_manager)?;
+    first_time::register_udf(func_manager)?;
+    first_val::register_udf(func_manager)?;
+    last_time::register_udf(func_manager)?;
+    last_val::register_udf(func_manager)?;
+    idelta_left::register_udf(func_manager)?;
+    idelta_right::register_udf(func_manager)?;
     Ok(())
 }
