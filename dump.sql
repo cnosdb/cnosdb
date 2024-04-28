559710a5e14ddb3a27363f6106a74a3e45fab7eb
h4ofanya <13824943566@163.com>
2024-04-28 16:32:30 +0800

    fix: read privilege can read resource_status

--- dump.sql
+++
@@ -1,9 +0,0 @@
--- Dump Global Object
-create tenant if not exists "test1";
--- Dump Tenant test1 Object
-\change_tenant test1
--- Dump Global Object
-create tenant if not exists "test2";
--- Dump Tenant test2 Object
-\change_tenant test2
-
