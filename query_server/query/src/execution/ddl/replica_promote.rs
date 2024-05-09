f41f550e748258fb427ef16f1173ebb771b9b3a9
yanyunliu <bart.liu@cnosdb.com>
2024-05-09 14:32:33 +0800

    implement replica prmote sql command

--- query_server/query/src/execution/ddl/replica_promote.rs
+++ query_server/query/src/execution/ddl/replica_promote.rs
@@ -1,7 +1,8 @@
 use async_trait::async_trait;
+use coordinator::ReplicationCmdType;
 use spi::query::execution::{Output, QueryStateMachineRef};
 use spi::query::logical_planner::ReplicaPromote;
-use spi::Result;
+use spi::{QueryError, Result};
 
 use super::DDLDefinitionTask;
 
@@ -18,8 +19,24 @@ impl ReplicaPromoteTask {
 
 #[async_trait]
 impl DDLDefinitionTask for ReplicaPromoteTask {
-    async fn execute(&self, _query_state_machine: QueryStateMachineRef) -> Result<Output> {
-        println!("-----------promote: {:?}", self.stmt);
-        todo!()
+    async fn execute(&self, query_state_machine: QueryStateMachineRef) -> Result<Output> {
+        let (replica_id, node_id) = (self.stmt.replica_id, self.stmt.node_id);
+        let tenant = query_state_machine.session.tenant();
+
+        let meta = query_state_machine.meta.clone();
+        let coord = query_state_machine.coord.clone();
+        let all_info = coordinator::get_replica_all_info(meta, tenant, replica_id).await?;
+
+        if let Some(info) = all_info.replica_set.by_node_id(node_id) {
+            let cmd_type = ReplicationCmdType::PromoteLeader(replica_id, info.id);
+            coord.replication_manager(tenant, cmd_type).await?;
+
+            Ok(Output::Nil(()))
+        } else {
+            Err(QueryError::ReplicaNotFound {
+                replica_id,
+                node_id,
+            })
+        }
     }
 }
