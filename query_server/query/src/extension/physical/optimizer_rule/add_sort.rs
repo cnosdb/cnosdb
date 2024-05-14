333a982002c794fa8d083adf4dd5565470fbc6c8
lutengda <18346072982@163.com>
2024-05-14 16:23:34 +0800

    refactor: When the gauge agg is recognized, SortExec plan is applied on the scan

--- query_server/query/src/extension/physical/optimizer_rule/add_sort.rs
+++ query_server/query/src/extension/physical/optimizer_rule/add_sort.rs
@@ -8,9 +8,12 @@ use datafusion::logical_expr::expr::Sort;
 use datafusion::logical_expr::{col, Expr};
 use datafusion::physical_optimizer::PhysicalOptimizerRule;
 use datafusion::physical_plan::aggregates::AggregateExec;
+use datafusion::physical_plan::sorts::sort_preserving_merge::SortPreservingMergeExec;
+use datafusion::physical_plan::udaf::AggregateFunctionExpr;
 use datafusion::physical_plan::ExecutionPlan;
 use datafusion::physical_planner::create_physical_sort_expr;
 
+use crate::extension::physical::plan_node::tskv_exec::TskvExec;
 use crate::extension::utils::downcast_execution_plan;
 
 #[non_exhaustive]
@@ -19,6 +22,33 @@ pub struct AddSortExec {}
 impl AddSortExec {
     pub fn new() -> Self {
         Self {}
+    }
+
+    fn optimize_inner(&self, plan: Arc<dyn ExecutionPlan>) -> DFResult<Arc<dyn ExecutionPlan>> {
+        plan.transform_up(&|plan| {
+            if let Some(tskv_exec) = downcast_execution_plan::<TskvExec>(plan.as_ref()) {
+                let physical_sort_expr = create_physical_sort_expr(
+                    &Expr::Sort(Sort::new(Box::new(col("time")), true, false)),
+                    &tskv_exec.schema().to_dfschema()?,
+                    &tskv_exec.schema(),
+                    &ExecutionProps::new(),
+                )?;
+                let sort_plan = Arc::new(
+                    datafusion::physical_plan::sorts::sort::SortExec::new(
+                        vec![physical_sort_expr.clone()],
+                        plan.clone(),
+                    )
+                    .with_preserve_partitioning(true),
+                );
+                let sort_merge_plan = Arc::new(SortPreservingMergeExec::new(
+                    vec![physical_sort_expr],
+                    sort_plan,
+                ));
+                return Ok(Transformed::Yes(sort_merge_plan));
+            }
+
+            Ok(Transformed::No(plan))
+        })
     }
 }
 
@@ -38,31 +68,19 @@ impl PhysicalOptimizerRule for AddSortExec {
             if let Some(agg_exec) = downcast_execution_plan::<AggregateExec>(plan.as_ref()) {
                 let mut is_need_sort = false;
                 agg_exec.aggr_expr().iter().for_each(|expr| {
-                    if !expr.support_concurrency() {
-                        is_need_sort = true;
+                    if let Some(agg_func_expr) =
+                        expr.as_any().downcast_ref::<AggregateFunctionExpr>()
+                    {
+                        if agg_func_expr.fun().name == "stats_agg"
+                            || agg_func_expr.fun().name == "gauge_agg"
+                        {
+                            is_need_sort = true;
+                        }
                     }
                 });
+
                 if is_need_sort {
-                    let physical_sort_expr = create_physical_sort_expr(
-                        &Expr::Sort(Sort::new(Box::new(col("time")), true, false)),
-                        &agg_exec.input().schema().to_dfschema()?,
-                        &agg_exec.input().schema(),
-                        &ExecutionProps::new(),
-                    )?;
-                    let input = Arc::new(datafusion::physical_plan::sorts::sort::SortExec::new(
-                        vec![physical_sort_expr],
-                        agg_exec.input().clone(),
-                    ));
-                    let plan = Arc::new(AggregateExec::try_new(
-                        *agg_exec.mode(),
-                        agg_exec.group_expr().clone(),
-                        agg_exec.aggr_expr().to_vec(),
-                        agg_exec.filter_expr().to_vec(),
-                        agg_exec.order_by_expr().to_vec(),
-                        input.clone(),
-                        input.schema(),
-                    )?);
-                    return Ok(Transformed::Yes(plan));
+                    return Ok(Transformed::Yes(self.optimize_inner(plan.clone())?));
                 }
             }
 
