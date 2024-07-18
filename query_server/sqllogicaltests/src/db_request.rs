28d902937ac3af5dadaea97041ba27051d322a7d
zipper-meng <92921679+zipper-meng@users.noreply.github.com>
2024-07-18 19:02:21 +0800

    feat(sqllogicaltests): add request type: HTTP for case http_metrics (#2231)

--- query_server/sqllogicaltests/src/db_request.rs
+++ query_server/sqllogicaltests/src/db_request.rs
@@ -1,8 +1,6 @@
 use std::path::Path;
 use std::str::FromStr;
 
-use arrow::datatypes::Schema;
-use arrow::record_batch::RecordBatch;
 use nom::branch::alt;
 use nom::bytes::complete::{tag, tag_no_case};
 use nom::character::complete::{alpha1, alphanumeric1, none_of, space0};
@@ -13,14 +11,31 @@ use nom::IResult;
 
 use crate::error::SqlError;
 use crate::instance::{
-    run_lp_write, run_open_tsdb_json_write, run_open_tsdb_write, run_query, CreateOptions,
-    SqlClientOptions,
+    run_http_api_v1_sql, run_lp_write, run_open_tsdb_json_write, run_open_tsdb_write, run_query,
+    CnosDBColumnType, CreateOptions, SqlClientOptions,
 };
+use crate::utils::normalize;
 
 type Result<T, E = SqlError> = std::result::Result<T, E>;
 
+pub struct DBRequest {
+    protocol: Protocol,
+    body: RequestBody,
+}
+
+impl DBRequest {
+    pub fn new(protocol: Protocol, body: RequestBody) -> Self {
+        Self { protocol, body }
+    }
+}
+
+pub enum Protocol {
+    Default,
+    Http,
+}
+
 #[derive(Clone, Debug)]
-pub enum DBRequest {
+pub enum RequestBody {
     Sql(String),
     LineProtocol(String),
     OpenTSDBProtocol(String),
@@ -86,23 +101,23 @@ impl DBRequest {
             .filter(|l| !l.trim().is_empty())
             .collect::<Vec<_>>();
         let mut sql = vec![];
+        let mut protocol = Protocol::Default;
         for (i, line) in lines.iter().enumerate() {
             let line = *line;
             if instruction_match("LP_BEGIN", line) {
-                return Ok(DBRequest::LineProtocol(parse_block(
-                    &lines[i + 1..],
-                    "LP_END",
-                )?));
+                let block = parse_block(&lines[i + 1..], "LP_END")?;
+                return Ok(DBRequest::new(protocol, RequestBody::LineProtocol(block)));
             } else if instruction_match("OPENTSDB_BEGIN", line) {
-                return Ok(DBRequest::OpenTSDBProtocol(parse_block(
-                    &lines[i + 1..],
-                    "OPENTSDB_END",
-                )?));
+                let block = parse_block(&lines[i + 1..], "OPENTSDB_END")?;
+                return Ok(DBRequest::new(
+                    protocol,
+                    RequestBody::OpenTSDBProtocol(block),
+                ));
             } else if instruction_match("OPENTSDB_JSON_BEGIN", line) {
-                return Ok(DBRequest::OpenTSDBJson(parse_block(
-                    &lines[i + 1..],
-                    "OPENTSDB_JSON_END",
-                )?));
+                let block = parse_block(&lines[i + 1..], "OPENTSDB_JSON_END")?;
+                return Ok(DBRequest::new(protocol, RequestBody::OpenTSDBJson(block)));
+            } else if instruction_match("HTTP", line) {
+                protocol = Protocol::Http;
             } else if instruction_match("", line) {
                 options.parse_and_change(line)
             } else {
@@ -110,9 +125,9 @@ impl DBRequest {
             }
         }
         if sql.is_empty() {
-            Ok(DBRequest::Nothing)
+            Ok(DBRequest::new(protocol, RequestBody::Nothing))
         } else {
-            Ok(DBRequest::Sql(sql.join("\n")))
+            Ok(DBRequest::new(protocol, RequestBody::Sql(sql.join("\n"))))
         }
     }
 
@@ -121,40 +136,69 @@ impl DBRequest {
         options: &SqlClientOptions,
         create_option: &CreateOptions,
         path: &Path,
-    ) -> Result<(Schema, Vec<RecordBatch>)> {
-        match self {
-            DBRequest::Sql(sql) => {
-                println!("[{}] Execute Sql: \"{}\"", path.display(), sql);
-                Ok(run_query(options, create_option, sql).await?)
-            }
-            DBRequest::LineProtocol(lp) => {
+    ) -> Result<(Vec<CnosDBColumnType>, Vec<Vec<String>>)> {
+        match &self.body {
+            RequestBody::Sql(sql) => match self.protocol {
+                Protocol::Default => {
+                    self.execute_record_batch(options, create_option, path, sql)
+                        .await
+                }
+                Protocol::Http => self.execute_http(options, path, sql).await,
+            },
+            RequestBody::LineProtocol(lp) => {
                 println!("[{}] Execute LineProtocol: \"{}\"", path.display(), lp);
                 run_lp_write(options, lp).await?;
-                Ok((Schema::empty(), vec![]))
+                Ok((vec![], vec![]))
             }
-            DBRequest::OpenTSDBProtocol(open_tsdb) => {
+            RequestBody::OpenTSDBProtocol(open_tsdb) => {
                 println!(
                     "[{}] Execute OpenTSDBProtocol: \"{}\"",
                     path.display(),
                     open_tsdb
                 );
                 run_open_tsdb_write(options, open_tsdb).await?;
-                Ok((Schema::empty(), vec![]))
+                Ok((vec![], vec![]))
             }
-            DBRequest::OpenTSDBJson(open_tsdb_json) => {
+            RequestBody::OpenTSDBJson(open_tsdb_json) => {
                 println!(
                     "[{}] Execute OpenTSDBJsonProtocol: \"{}\"",
                     path.display(),
                     open_tsdb_json
                 );
                 run_open_tsdb_json_write(options, open_tsdb_json).await?;
-                Ok((Schema::empty(), vec![]))
+                Ok((vec![], vec![]))
             }
-            DBRequest::Nothing => {
+            RequestBody::Nothing => {
                 println!("[{}] Execute Nothing", path.display());
-                Ok((Schema::empty(), vec![]))
+                Ok((vec![], vec![]))
             }
         }
+    }
+
+    async fn execute_record_batch(
+        &self,
+        options: &SqlClientOptions,
+        create_option: &CreateOptions,
+        path: &Path,
+        sql: &str,
+    ) -> Result<(Vec<CnosDBColumnType>, Vec<Vec<String>>)> {
+        println!("[{}] Execute Sql (flight): \"{}\"", path.display(), sql);
+        let (schema, batches) = run_query(options, create_option, sql).await?;
+        let types = normalize::convert_schema_to_types(schema.fields());
+        let rows = normalize::convert_batches(batches)?;
+        Ok((types, rows))
+    }
+
+    async fn execute_http(
+        &self,
+        options: &SqlClientOptions,
+        path: &Path,
+        sql: &str,
+    ) -> Result<(Vec<CnosDBColumnType>, Vec<Vec<String>>)> {
+        println!("[{}] Execute Sql (http): \"{}\"", path.display(), sql);
+        let rows = run_http_api_v1_sql(options, sql).await?;
+        let cols_num = rows.first().map(|c| c.len()).unwrap_or(0);
+        Ok((vec![CnosDBColumnType::Text; cols_num], rows))
     }
 }
 
