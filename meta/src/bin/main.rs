ccdb0b7c23fbd10e3d036ff54a094618bcc64f49
civi1007 <1958358577@qq.com>
2024-09-23 17:36:11 +0800

    add command 'cnosdb-meta config' to view the default configuration of cnosdb-meta

--- meta/src/bin/main.rs
+++ meta/src/bin/main.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code, clippy::field_reassign_with_default)]
 
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 use config::VERSION;
 use meta::signal;
 use trace::global_logging::init_global_logging;
@@ -8,15 +8,33 @@ use trace::global_logging::init_global_logging;
 #[derive(Debug, Parser)]
 #[command(name = "cnosdb-meta", version = & VERSION[..])]
 struct Cli {
-    /// configuration path
+    #[command(subcommand)]
+    subcmd: CliCommand,
+}
+
+#[derive(Debug, Subcommand)]
+enum CliCommand {
+    /// Run CnosDB meta server.
+    Run(MetaConfig),
+    /// Print default configurations.
+    Config,
+}
+#[derive(Debug, Args)]
+struct MetaConfig {
     #[arg(short, long)]
     config: Option<String>,
 }
-
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), std::io::Error> {
     let cli = Cli::parse();
-    let mut options = config::meta::get_opt(cli.config);
+    let run_args = match cli.subcmd {
+        CliCommand::Run(run_args) => run_args,
+        CliCommand::Config => {
+            println!("{}", config::meta::Opt::default().to_string_pretty());
+            return Ok(());
+        }
+    };
+    let mut options = config::meta::get_opt(run_args.config);
     println!("-----------------------------------------------------------");
     println!("Start meta server with configuration:");
     println!("{}", options.to_string_pretty());
@@ -29,4 +47,5 @@ async fn main() {
         .unwrap();
 
     signal::block_waiting_ctrl_c();
+    Ok(())
 }
