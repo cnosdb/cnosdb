ace49843b9a42cb70fcc6a2661dc89ecb49263dd
paradox0503 <jialinhan123@126.com>
2024-10-11 10:05:01 +0800

    fix:prom remote read api unable to query upper case table name problem

--- e2e_test/src/reliant/prom.rs
+++ e2e_test/src/reliant/prom.rs
@@ -43,6 +43,19 @@ fn labels() -> Vec<Label> {
     vec![table_name_label, tag_label]
 }
 
+fn labels_upper() -> Vec<Label> {
+    let table_name_label = Label {
+        name: "__name__".to_string(),
+        value: "TEST_prom".to_string(),
+    };
+
+    let tag_label = Label {
+        name: "tag1".to_string(),
+        value: "todo!()".to_string(),
+    };
+    vec![table_name_label, tag_label]
+}
+
 fn test_write_req() -> Vec<u8> {
     let sample = Sample {
         value: 1.1,
@@ -63,11 +76,58 @@ fn test_write_req() -> Vec<u8> {
     serialize(write_request)
 }
 
+fn test_write_upper_req() -> Vec<u8> {
+    let sample = Sample {
+        value: 1.1,
+        timestamp: 1686819776617,
+    };
+
+    let timeseries = TimeSeries {
+        labels: labels_upper(),
+        samples: vec![sample],
+        ..Default::default()
+    };
+
+    let write_request = WriteRequest {
+        timeseries: vec![timeseries],
+        ..Default::default()
+    };
+
+    serialize(write_request)
+}
+
 fn test_read_req() -> Vec<u8> {
     let table_name_label_matcher = LabelMatcher {
         r#type: label_matcher::Type::Eq.into(),
         name: "__name__".to_string(),
         value: "test_prom".to_string(),
+    };
+    let label_matcher = LabelMatcher {
+        r#type: label_matcher::Type::Eq.into(),
+        name: "tag1".to_string(),
+        value: "todo!()".to_string(),
+    };
+
+    let query = Query {
+        start_timestamp_ms: 1686819776615,
+        end_timestamp_ms: 1686819777615,
+        matchers: vec![table_name_label_matcher, label_matcher],
+        ..Default::default()
+    };
+
+    let read_req = ReadRequest {
+        queries: vec![query],
+        ..Default::default()
+    };
+
+    serialize(read_req)
+}
+
+fn test_read_upper_req() -> Vec<u8> {
+    let table_name_label_matcher = LabelMatcher {
+        r#type: label_matcher::Type::Eq.into(),
+        name: "__name__".to_string(),
+        value: "TEST_prom".to_string(),
     };
     let label_matcher = LabelMatcher {
         r#type: label_matcher::Type::Eq.into(),
@@ -110,6 +170,26 @@ fn test_read_resp() -> ReadResponse {
         results: vec![result],
     }
 }
+fn test_read_upper_resp() -> ReadResponse {
+    let sample = Sample {
+        value: 1.1,
+        timestamp: 1686819776617,
+    };
+
+    let timeseries = TimeSeries {
+        labels: labels_upper(),
+        samples: vec![sample],
+        ..Default::default()
+    };
+
+    let result = QueryResult {
+        timeseries: vec![timeseries],
+    };
+
+    ReadResponse {
+        results: vec![result],
+    }
+}
 
 #[test]
 fn test_prom() {
@@ -118,9 +198,21 @@ fn test_prom() {
     // clean data
     let body = "drop table if exists test_prom;";
     check_response!(client.post("http://127.0.0.1:8902/api/v1/sql?db=public", body));
+    let body = "drop table if exists TEST_upper;";
+    check_response!(client.post("http://127.0.0.1:8902/api/v1/sql?db=public", body));
 
     // write data
     let body = test_write_req();
+    check_response!(client
+        .request_with_auth(
+            Method::POST,
+            "http://127.0.0.1:8902/api/v1/prom/write?db=public",
+        )
+        .body(body)
+        .send());
+
+    //upper name test
+    let body = test_write_upper_req();
     check_response!(client
         .request_with_auth(
             Method::POST,
@@ -144,4 +236,19 @@ fn test_prom() {
 
     let resp: ReadResponse = deserialize(&resp.bytes().unwrap());
     assert_eq!(resp, test_read_resp());
+
+    // read upper tablename data
+    let body = test_read_upper_req();
+    let resp = client
+        .request_with_auth(
+            Method::POST,
+            "http://127.0.0.1:8902/api/v1/prom/read?db=public",
+        )
+        .body(body)
+        .send()
+        .unwrap();
+    assert_eq!(resp.status(), status_code::OK);
+
+    let resp: ReadResponse = deserialize(&resp.bytes().unwrap());
+    assert_eq!(resp, test_read_upper_resp());
 }
--- query_server/query/src/prom/remote_server.rs
+++ query_server/query/src/prom/remote_server.rs
@@ -309,7 +309,7 @@ fn build_sql_with_table(
         .into_iter()
         .map(|table| SqlWithTable {
             sql: format!(
-                "SELECT * FROM {} WHERE {}",
+                "SELECT * FROM \"{}\" WHERE {}",
                 table.name,
                 filters.join(" AND ")
             ),
