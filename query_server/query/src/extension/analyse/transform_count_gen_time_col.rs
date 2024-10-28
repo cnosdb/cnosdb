e889c0d064c921e92c01b528e214296b0d1bfe2b
lutengda <18346072982@163.com>
2024-10-28 15:52:57 +0800

    fix: "transform_count_gen_time_col" rule not apply to external table

--- query_server/query/src/extension/analyse/transform_count_gen_time_col.rs
+++ query_server/query/src/extension/analyse/transform_count_gen_time_col.rs
@@ -1,10 +1,13 @@
 use datafusion::common::tree_node::{Transformed, TreeNode};
 use datafusion::config::ConfigOptions;
+use datafusion::datasource::source_as_provider;
 use datafusion::error::Result;
 use datafusion::logical_expr::expr::AggregateFunction;
 use datafusion::logical_expr::{aggregate_function, Aggregate, LogicalPlan};
 use datafusion::optimizer::analyzer::AnalyzerRule;
 use datafusion::prelude::Expr;
+
+use crate::data_source::batch::tskv::ClusterTable;
 
 pub struct TransformCountGenTimeColRule {}
 
@@ -39,21 +42,29 @@ fn analyze_internal(plan: LogicalPlan) -> Result<Transformed<LogicalPlan>> {
                             let last = plan_vec.last().unwrap().clone();
                             match last {
                                 LogicalPlan::TableScan(scan) => {
-                                    // add time column to projection
-                                    let mut new_projection =
-                                        scan.projection.clone().unwrap_or_default();
-                                    new_projection.insert(0, 0_usize);
-                                    let mut new_scan = scan.clone();
-                                    new_scan.projection = Some(new_projection);
+                                    if source_as_provider(&scan.source)?
+                                        .as_any()
+                                        .downcast_ref::<ClusterTable>()
+                                        .is_some()
+                                    {
+                                        // add time column to projection
+                                        let mut new_projection =
+                                            scan.projection.clone().unwrap_or_default();
+                                        new_projection.insert(0, 0_usize);
+                                        let mut new_scan = scan.clone();
+                                        new_scan.projection = Some(new_projection);
 
-                                    // change plan
-                                    plan_vec.pop();
-                                    let mut new_plan = LogicalPlan::TableScan(new_scan);
-                                    while let Some(last) = plan_vec.pop() {
-                                        new_plan = last.with_new_inputs(&[new_plan])?;
+                                        // change plan
+                                        plan_vec.pop();
+                                        let mut new_plan = LogicalPlan::TableScan(new_scan);
+                                        while let Some(last) = plan_vec.pop() {
+                                            new_plan = last.with_new_inputs(&[new_plan])?;
+                                        }
+
+                                        return Ok(Transformed::Yes(new_plan));
+                                    } else {
+                                        return Ok(Transformed::No(plan.clone()));
                                     }
-
-                                    return Ok(Transformed::Yes(new_plan));
                                 }
                                 LogicalPlan::Join(_) | LogicalPlan::CrossJoin(_) => {
                                     return Ok(Transformed::No(plan.clone()));
