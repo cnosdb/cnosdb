88f97e6e0c6cc54b4ec4a56d57cac5cce129b32e
Subsegment <304741833@qq.com>
2024-11-08 11:24:19 +0800

    chore: check tsm overlap when chunk merge

--- tskv/src/reader/mod.rs
+++ tskv/src/reader/mod.rs
@@ -308,14 +308,7 @@ impl PartialOrd<Self> for DataReference {
 
 impl Ord for DataReference {
     fn cmp(&self, other: &Self) -> Ordering {
-        match (self, other) {
-            (DataReference::Chunk(_, _, _), DataReference::Memcache(_, _, _)) => Ordering::Less,
-            (DataReference::Memcache(_, _, _), DataReference::Chunk(_, _, _)) => Ordering::Greater,
-            (DataReference::Chunk(_, _, f1), DataReference::Chunk(_, _, f2)) => {
-                f1.file_id().cmp(&f2.file_id())
-            }
-            (DataReference::Memcache(_, _, c1), DataReference::Memcache(_, _, c2)) => c1.cmp(c2),
-        }
+        self.file_id().cmp(&other.file_id())
     }
 }
 
@@ -324,6 +317,13 @@ impl DataReference {
         match self {
             DataReference::Chunk(chunk, ..) => *chunk.time_range(),
             DataReference::Memcache(_, trs, ..) => trs.max_time_range(),
+        }
+    }
+
+    pub fn file_id(&self) -> ColumnFileId {
+        match self {
+            DataReference::Chunk(_, _, cf) => cf.file_id(),
+            DataReference::Memcache(_, _, cf_id) => *cf_id,
         }
     }
 }
